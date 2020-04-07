3a7081bb89493e3fd186de6d1e59034643f282d0
MerrinX <knutago1@outlook.com>
2020-04-07 21:38:20 +0200

    screens

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -9,7 +9,7 @@ is_running() {
 }
 
 # Set Monitor posistion
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 3000x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 1920x1080 --pos 1080x0 --rotate normal
 
 # Set the background image
 # feh --bg-fill /home/merrinx/Pictures/Wallpapers/abstract_face.jpg &
