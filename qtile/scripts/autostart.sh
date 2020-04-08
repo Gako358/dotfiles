db5c69fc9a108ce85455a28a4d3996f7ddfc9203
MerrinX <knutago1@outlook.com>
2020-04-08 22:54:02 +0200

    Updated bg

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -13,8 +13,12 @@ xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 3000x0 --ro
 
 # Set the background image
 # feh --bg-fill /home/merrinx/Pictures/Wallpapers/abstract_face.jpg &
+# feh --bg-fill /home/merrinx/Pictures/Wallpapers/abstract_earth.jpg &
 # feh --bg-fill /home/merrinx/Pictures/Wallpapers/digital_sky.jpg &
-feh --bg-fill /home/merrinx/Pictures/Wallpapers/digital_forrest.jpg &
+# feh --bg-fill /home/merrinx/Pictures/Wallpapers/digital_forrest.jpg &
+# feh --bg-fill /home/merrinx/Pictures/Wallpapers/digital_streets.jpg &
+# feh --bg-scale --no-xinerama /home/merrinx/Pictures/Wallpapers/xinerama_earth.jpg &
+feh --bg-scale --no-xinerama /home/merrinx/Pictures/Wallpapers/xinerama_landscape.jpg &
 
 # Wait to let the X-Session start up correctly
 sleep 1
@@ -27,3 +31,5 @@ fi;
 # Network manager
 [[ $(is_running 'nm-applet') ]] || nm-applet &
 
+# Icue Corsair
+/usr/bin/ckb-next --background &
