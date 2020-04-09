46459dcd430ae3272040aece3e89d50a788a6459
MerrinX <knutago1@outlook.com>
2020-04-09 19:48:37 +0200

    layout

--- standard.py
+++ standard.py
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1080x840 --rotate normal --output DP-2 --off --output HDMI-1 --mode 1920x1080 --pos 3000x840 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate right
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 3000x840 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --primary --mode 1920x1080 --pos 1080x840 --rotate normal
