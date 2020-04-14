8b3f48fb7da2c7f7c2600b60e1f8be3329ac7d13
MerrinX <knutago1@outlook.com>
2020-04-14 22:15:26 +0200

    removed startup delay

--- qtile/config.py
+++ qtile/config.py
@@ -29,7 +29,6 @@ def autostart():
     state from the rest of the init. This will cause start/restart of qtile
     to hang slightly as the sleep runs.
     """
-    os.environ.setdefault('RUNNING_QTILE', 'True')
     run_script("autostart.sh")
 
 @hook.subscribe.client_new
