304b87c1af9df5e578f7da70644fba12e0074ad9
MerrinX <knutago1@outlook.com>
2020-04-23 18:22:56 +0200

    update dotfiles

--- .screenlayout/login.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1080x840 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 3000x840 --rotate normal
