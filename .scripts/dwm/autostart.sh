e1c76c948cf53d114ca1877b001310cb8a06aaf2
MerrinX <knutago1@outlook.com>
2020-04-28 13:26:39 +0200

    Updated for install

--- .scripts/dwm/autostart.sh
+++ .scripts/dwm/autostart.sh
@@ -2,6 +2,9 @@
 picom -b -d :0 &
 feh --bg-fill /home/merrinx/Pictures/Wallpapers/xinerama_landscape.jpg &
 
+# Start slstatus bar
+/usr/local/bin/slstatus &
+
 # Autostart Apps
 chromium &
 discord &
