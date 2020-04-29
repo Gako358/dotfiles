2b2eb20d9e25d62c72f7a1c36dd8f5a0c410a2cd
MerrinX <knutago1@outlook.com>
2020-04-29 13:29:15 +0000

    dwmblocks added

--- .scripts/dwm/autostart.sh
+++ .scripts/dwm/autostart.sh
@@ -3,7 +3,7 @@ picom -b -d :0 &
 feh --bg-fill /home/merrinx/Pictures/Wallpapers/xinerama_landscape.jpg &
 
 # Start slstatus bar
-/usr/local/bin/slstatus &
+/usr/local/bin/dwmblocks &
 
 # Autostart Apps
 chromium &
