27b17f5565b5a855757a20d837cb17f0ad366faa
Knut Øien <gako358@outlook.com>
2020-10-11 10:13:37 +0200

    updated fish functions

---
+++ fish/functions/t22.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function t22 --wraps='cd ~/Projects/Tutorials/Assembly/' --description 'alias t22 cd ~/Projects/Tutorials/Assembly/'
+  cd ~/Projects/Tutorials/Assembly/ $argv;
+end
