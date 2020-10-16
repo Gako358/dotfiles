0d5caaccdd78922095e15abd7e81fd3a2d3dc784
Gako358 <gako358@outlook.com>
2020-10-16 19:30:48 +0200

    added remote git check

--- fish/functions/gremote.fish
+++ fish/functions/gremote.fish
@@ -1,51 +1,51 @@
 function gremote --description 'git remote'
-    echo Checking Dotfiles
-    echo --------------------
+    echo (set_color brred) Checking Dotfiles (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Sources/Dotfiles/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Secrets
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Secrets (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Sources/Secrets/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Documents
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Documents (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Documents/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Suckless
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Suckless (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Projects/Suckless/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Courses
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Courses (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Projects/Courses/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Algorithms
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Algorithms (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Projects/Algorithms/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Masterclass
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Masterclass (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Projects/Masterclass/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
-    echo Checking Latest project
-    echo --------------------
+    echo (set_color brgreen)--------------------(set_color normal)
+    echo (set_color brred) Checking Latest project (set_color normal)
+    echo (set_color brgreen)--------------------(set_color normal)
     cd /home/merrinx/Projects/assignment-3-cache-johnnycach/ $argv
     git remote update $argv
     git status -uno $argv
-    echo---------------------
+    echo (set_color brgreen) ----------COMPLETE!---------- (set_color normal)
     cd /home/merrinx/ $argv
 end
--- fish/functions/gstatus.fish
+++ fish/functions/gstatus.fish
@@ -1,45 +1,43 @@
 function gstatus --description 'git status'
-    echo Checking Dotfiles
-    echo --------------------
+    echo (set_color brred) Checking Dotfiles (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Sources/Dotfiles/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Secrets
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Secrets (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Sources/Secrets/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Documents
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Documents (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Documents/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Suckless
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Suckless (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Projects/Suckless/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Courses
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Courses (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Projects/Courses/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Algorithms
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Algorithms (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Projects/Algorithms/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Masterclass
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Masterclass (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Projects/Masterclass/ $argv
     git status $argv
-    echo --------------------
-    echo Checking Latest project
-    echo --------------------
+    echo (set_color brgreen) -------------------- (set_color normal)
+    echo (set_color brred) Checking Latest Project (set_color normal)
+    echo (set_color brgreen) -------------------- (set_color normal)
     cd /home/merrinx/Projects/assignment-3-cache-johnnycach/ $argv
     git status $argv
-    echo --------------------
-    echo COMPLETE!
-    echo --------------------
+    echo (set_color brgreen) ----------COMPLETE!---------- (set_color normal)
     cd /home/merrinx/ $argv
 end
