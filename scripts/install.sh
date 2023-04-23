33e8dc5ff0865fb738f7b544f4296ab3824a9091
Gako358 <gako358@outlook.com>
2023-04-23 14:10:12 +0200

    fix install script work

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,221 +1,109 @@
 #!/usr/bin/env bash
 
-function mkfs_desktop
-{
-  #Create partitions
-  parted --script -a optimal /dev/nvme0n1 'mklabel gpt' & pid1=$!
-  wait $pid1
-  parted --script -a optimal /dev/nvme0n1 'mkpart efi 0% 512MiB' & pid2=$!
-  wait $pid2
-  parted --script -a optimal /dev/nvme0n1 'mkpart swap 512MiB 37GiB' & pid3=$!
-  wait $pid3
-  parted --script -a optimal /dev/nvme0n1 'mkpart nixos 37GiB 100%' & pid4=$!
-  wait $pid4
-  parted --script -a optimal /dev/nvme0n1 'set 1 boot on' & pid5=$!
-  wait $pid5
-  parted --script -a optimal /dev/nvme1n1 'mklabel gpt' & pid6=$!
-  wait $pid6
-  parted --script -a optimal /dev/nvme1n1 'mkpart nix 0% 100%' & pid7=$!
-  wait $pid7
-  parted --script -a optimal /dev/sda 'mklabel gpt' & pid8=$!
-  wait $pid8
-  parted --script -a optimal /dev/sda 'mkpart arch 0% 100%' & pid9=$!
-  wait $pid9
-  parted --script -a optimal /dev/sdb 'mklabel gpt' & pid10=$!
-  wait $pid10
-  parted --script -a optimal /dev/sdb 'mkpart opt 0% 100%' & pid11=$!
-  wait $pid11
+function create_partitions() {
+  local device=$1
+  local efi_size=$2
+  local swap_size=$3
+  local nixos_size=$4
+  local boot_label=$5
+  local swap_label=$6
+  local nixos_label=$7
+  local nix_label=$8
+  local additional_devices=$9
 
-  clear
-  echo "Partitions created"
+  # Create partitions
+  parted --script -a optimal "$device" 'mklabel gpt'
+  parted --script -a optimal "$device" "mkpart $boot_label 0% $efi_size"
+  parted --script -a optimal "$device" "mkpart $swap_label $efi_size $swap_size"
+  parted --script -a optimal "$device" "mkpart $nixos_label $swap_size $nixos_size"
+  parted --script -a optimal "$device" 'set 1 boot on'
 
-  # Print table
-  lsblk
+  # Format partitions
+  mkfs.fat -F32 -n EFI "${device}p1"
+  mkswap -L SWAP "${device}p2"
+  mkfs.btrfs -L NIXOS -f "${device}p3"
+
+  if [ -n "$nix_label" ]; then
+    local nix_device="$9"
+    parted --script -a optimal "$nix_device" "mkpart $nix_label $nixos_size 100%"
+    mkfs.btrfs -L NIX -f "${nix_device}p1"
+  fi
+
+  if [ -n "$additional_devices" ]; then
+    for partition in "${additional_devices[@]}"; do
+      parted --script -a optimal "${partition[4]}" "mkpart ${partition[0]} ${partition[1]} ${partition[2]}"
+      mkfs.btrfs -L "${partition[0]}" -f "${partition[4]}p${partition[3]}"
+    done
+  fi
 }
 
-function mkfs_desktop_simple
-{
-  Create partitions, without sda1 and sdb1
-  parted --script -a optimal /dev/nvme0n1 'mklabel gpt' & pid1=$!
-  wait $pid1
-  parted --script -a optimal /dev/nvme0n1 'mkpart efi 0% 512MiB' & pid2=$!
-  wait $pid2
-  parted --script -a optimal /dev/nvme0n1 'mkpart swap 512MiB 37GiB' & pid3=$!
-  wait $pid3
-  parted --script -a optimal /dev/nvme0n1 'mkpart nixos 37GiB 100%' & pid4=$!
-  wait $pid4
-  parted --script -a optimal /dev/nvme0n1 'set 1 boot on' & pid5=$!
-  wait $pid5
-  parted --script -a optimal /dev/nvme1n1 'mklabel gpt' & pid6=$!
-  wait $pid6
-  parted --script -a optimal /dev/nvme1n1 'mkpart nix 0% 100%' & pid7=$!
-  wait $pid7
+function mount_partitions() {
+  local device=$1
+  local nix_label=$2
+  local nix_device=$3
+  shift 3
+  local additional_partitions=("$@")
+  local nixos_subvolumes=(root home tmp)
+  local mount_options="noatime,compress=zstd,ssd,space_cache=v2"
 
-  clear
-  echo "Partitions created"
+  mount "${device}p3" /mnt
 
-  # Print table
-  lsblk
-
-}
-
-function mkfs_laptop
-{
-  #Create partitions
-  parted --script -a optimal /dev/sda 'mklabel gpt' & pid1=$!
-  wait $pid1
-  parted --script -a optimal /dev/sda 'mkpart efi 0% 512MiB' & pid2=$!
-  wait $pid2
-  parted --script -a optimal /dev/sda 'mkpart swap 512MiB 9.1GiB' & pid3=$!
-  wait $pid3
-  parted --script -a optimal /dev/sda 'mkpart nixos 9.1GiB 100%' & pid4=$!
-  wait $pid4
-  parted --script -a optimal /dev/sda 'set 1 boot on' & pid5=$!
-  wait $pid5
-
-  clear
-  echo "Partitions created"
-
-  # Print table
-  lsblk
-}
-
-function setup_desktop
-{
-  # Format Partitions
-  mkfs.fat -F32 -n EFI /dev/nvme0n1p1 & pid12=$!
-  wait $pid12
-  mkswap -L SWAP /dev/nvme0n1p2 & pid13=$!
-  wait $pid13
-  mkfs.btrfs -L NIXOS -f /dev/nvme0n1p3 & pid14=$!
-  wait $pid14
-  mkfs.btrfs -L NIX -f /dev/nvme1n1p1 & pid15=$!
-  wait $pid15
-  mkfs.ext4 -L ARCH /dev/sda1 & pid16=$!
-  wait $pid16
-  mkfs.xfs -L OPT -f /dev/sdb1 & pid17=$!
-  wait $pid17
-
-  echo "Partitions formatted"
-
-  # Mount partitions
-  mount /dev/nvme0n1p3 /mnt
-
-  btrfs subvolume create /mnt/root
-  btrfs subvolume create /mnt/home
-  btrfs subvolume create /mnt/tmp
+  for subvol in "${nixos_subvolumes[@]}"; do
+    btrfs subvolume create "/mnt/$subvol"
+  done
 
   umount /mnt
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=root /dev/nvme0n1p3 /mnt
 
-  mkdir -p /mnt/{boot/efi,home,tmp,nix,opt,arch}
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=home /dev/nvme0n1p3 /mnt/home
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=tmp /dev/nvme0n1p3 /mnt/tmp
+  mount -o "$mount_options",subvol=root "${device}p3" /mnt
 
-  mount -o noatime,compress=zstd,ssd,space_cache=v2 /dev/nvme1n1p1 /mnt/nix
-  mount /dev/sda1 /mnt/arch
-  mount /dev/sdb1 /mnt/opt
+  for subvol in "${nixos_subvolumes[@]}"; do
+    mkdir -p "/mnt/$subvol"
+    mount -o "$mount_options",subvol="$subvol" "${device}p3" "/mnt/$subvol"
+  done
 
-  mount /dev/nvme0n1p1 /mnt/boot/efi
-  swapon /dev/nvme0n1p2 & pid18=$!
-  wait $pid18
+  mkdir -p /mnt/boot/efi
+  mount "${device}p1" /mnt/boot/efi
+  swapon "${device}p2"
+
+  if [ -n "$nix_label" ]; then
+    mkdir -p /mnt/nix
+    mount -o "$mount_options" "${nix_device}p1" /mnt/nix
+  fi
+
+  if [ -n "$additional_partitions" ]; then
+    for partition_str in "${additional_partitions[@]}"; do
+      IFS=',' read -ra partition <<< "$partition_str"
+      local mount_point="/mnt/${partition[0]}"
+      mkdir -p "$mount_point"
+      mount -o "$mount_options" "${partition[4]}p${partition[3]}" "$mount_point"
+    done
+  fi
 
   clear
   df -Th
   free -h
-  echo "Run: nixos-install --flake github:gako358/Dotfiles#mXdesktop" 
+
+  echo "Partitions created and formatted"
 }
 
-function setup_desktop_simple
-{
-  # Format Partitions
-  mkfs.fat -F32 -n EFI /dev/nvme0n1p1 & pid12=$!
-  wait $pid12
-  mkswap -L SWAP /dev/nvme0n1p2 & pid13=$!
-  wait $pid13
-  mkfs.btrfs -L NIXOS -f /dev/nvme0n1p3 & pid14=$!
-  wait $pid14
-  mkfs.btrfs -L NIX -f /dev/nvme1n1p1 & pid15=$!
-  wait $pid15
-
-  echo "Partitions formatted"
-
-  # Mount partitions
-  mount /dev/nvme0n1p3 /mnt
-
-  btrfs subvolume create /mnt/root
-  btrfs subvolume create /mnt/home
-  btrfs subvolume create /mnt/tmp
-
-  umount /mnt
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=root /dev/nvme0n1p3 /mnt
-
-  mkdir -p /mnt/{boot/efi,home,tmp,nix,opt,arch}
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=home /dev/nvme0n1p3 /mnt/home
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=tmp /dev/nvme0n1p3 /mnt/tmp
-
-  mount -o noatime,compress=zstd,ssd,space_cache=v2 /dev/nvme1n1p1 /mnt/nix
-  mount /dev/sda1 /mnt/arch
-  mount /dev/sdb1 /mnt/opt
-
-  mount /dev/nvme0n1p1 /mnt/boot/efi
-  swapon /dev/nvme0n1p2 & pid18=$!
-  wait $pid18
-
-  clear
-  df -Th
-  free -h
-  echo "Run: nixos-install --flake github:gako358/Dotfiles#mXdesktop" 
+function setup_desktop() {
+  nix_label="nix"
+  additional_devices=(("opt" "0%" "100%" "1" "/dev/sda"), ("arch" "0%" "100%" "1" "/dev/sdb"))
+  create_partitions /dev/nvme0n1 512MiB 37GiB 100% efi swap nixos nix /dev/nvme1n1 additional_devices
+  mount_partitions /dev/nvme0n1 nix /dev/nvme1n1 "${additional_devices[@]}"
+  echo "Run: nixos-install --flake github:gako358/Dotfiles#terangreal"
 }
 
-function setup_laptop
-{
-  # Format Partitions
-  mkfs.fat -F32 -n EFI /dev/sda1 & pid6=$!
-  wait $pid6
-  mkswap -L SWAP /dev/sda2 & pid7=$!
-  wait $pid7
-  mkfs.btrfs -L NIXOS -f /dev/sda3 & pid8=$!
-  wait $pid8
-
-  echo "Partitions formatted"
-
-  # Mount partitions
-  mount /dev/sda3 /mnt
-
-  btrfs subvolume create /mnt/root
-  btrfs subvolume create /mnt/home
-  btrfs subvolume create /mnt/nix
-  btrfs subvolume create /mnt/var
-  btrfs subvolume create /mnt/tmp
-
-  umount /mnt
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=root /dev/sda3 /mnt
-
-  mkdir -p /mnt/{boot/efi,home,tmp,nix,var}
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=home /dev/sda3 /mnt/home
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=nix /dev/sda3 /mnt/nix
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=var /dev/sda3 /mnt/var
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=tmp /dev/sda3 /mnt/tmp
-
-  mount /dev/sda1 /mnt/boot/efi
-  swapon /dev/sda2 & pid9=$!
-  wait $pid9
-
-  clear
-  df -Th
-  free -h
-  echo "Run: nixos-install --flake github:gako358/Dotfiles#mXlaptop" 
+function setup_worklaptop() {
+  create_partitions /dev/nvme0n1 512MiB 9.1GiB 100% efi swap nixos
+  mount_partitions /dev/nvme0n1
+  echo "Run: nixos-install --flake github:gako358/Dotfiles#tuathaan"
 }
 
-function install_desktop() {
-  # Install
-  nixos-install --flake github:gako358/nixos#terangreal
-}
-
-function install_laptop() {
-  # Install
-  nixos-install --flake github:gako358/nixos#tuathaan
+function setup_homelaptop() {
+  create_partitions /dev/sda 512MiB 9.1GiB 100% efi swap nixos
+  mount_partitions /dev/sda
+  echo "Run: nixos-install --flake github:gako358/Dotfiles#tuathaan"
 }
 
 "$@"
