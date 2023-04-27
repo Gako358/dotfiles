0206acd511eeae56ea0f117e377d308dbee44068
gako358 <gako358@outlook.com>
2023-04-27 11:27:48 +0000

    update system, new java temp
    new system build, gcc

--- templates/java/flake.nix
+++ templates/java/flake.nix
@@ -10,8 +10,17 @@
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      jdtls = pkgs.callPackage ./.jdtls.nix {};
+      javaVersion = 8;
+
+      overlays = [
+        (self: super: rec {
+          jdk = super."jdk${toString javaVersion}";
+          maven = super.maven.override {
+            inherit jdk;
+          };
+        })
+      ];
+      pkgs = import nixpkgs {inherit overlays system;};
     in {
       devShell = let
         generateEditorConfig = pkgs.writeShellScriptBin "generateEditorConfig" ''
@@ -21,7 +30,6 @@
             echo "[*]" >> .editorconfig
             echo "end_of_line = lf" >> .editorconfig
             echo "insert_final_newline = true" >> .editorconfig
-            echo "charset = utf-8" >> .editorconfig
             echo "" >> .editorconfig
             echo "# 4 space indentation" >> .editorconfig
             echo "[*.{java,html,js,xml,jsp}]" >> .editorconfig
@@ -29,17 +37,29 @@
             echo "tab_width = 4" >> .editorconfig
           fi
         '';
+        mvnRunAll = pkgs.writeShellScriptBin "mvnRunAll" ''
+          mvn clean package -PsetLocal,runAllDb,importAll
+        '';
+        mvnP = pkgs.writeShellScriptBin "mvnP" ''
+          mvn package
+        '';
+        mvnCP = pkgs.writeShellScriptBin "mvnCP" ''
+          mvn clean package
+        '';
       in
         pkgs.mkShell {
           name = "java";
           buildInputs = with pkgs; [
+            # ${generateEditorConfig}/bin/generateEditorConfig
             jdk
-            jdtls
+            maven
             generateEditorConfig
+            mvnRunAll
+            mvnCP
+            mvnP
           ];
           shellHook = ''
             export JAVA_HOME=${pkgs.jdk}
-            ${generateEditorConfig}/bin/generateEditorConfig
           '';
         };
     });
