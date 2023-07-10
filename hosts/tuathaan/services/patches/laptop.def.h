d97d562c97d8717adff266a86358e790c0df5c6c
Gako358 <gako358@outlook.com>
2023-07-10 19:32:29 +0200

    dwm rule set change for teams and slack

--- hosts/tuathaan/services/patches/laptop.def.h
+++ hosts/tuathaan/services/patches/laptop.def.h
@@ -99,9 +99,9 @@ static const Rule rules[] = {
        monitor */
     {"Gimp", NULL, NULL, 0, 1, 1, -1},
     {"firefox", NULL, NULL, 1, 0, 0, -1},
-    {"thunderbird", NULL, NULL, 1 << 3, 1, 1, -1},
+    {"thunderbird", NULL, NULL, 1 << 3, 0, 0, -1},
     {"discord", NULL, NULL, 1 << 4, 0, 0, -1},
-    {"Slack", NULL, NULL, 1 << 4, 0, 0, -1},
+    {"Slack", NULL, NULL, 1 << 3, 0, 0, -1},
     {"Pcmanfm", NULL, NULL, 0, 0, 1, -1},
     {"Zathura", NULL, NULL, 0, 0, 1, -1},
 
