e8a22dc9424dd2e66c2b7027b3750cde05314d0b
Gako358 <gako358@outlook.com>
2023-07-24 08:54:23 +0200

    fix uuid for disk on laptop

--- hosts/tuathaan/disks.nix
+++ hosts/tuathaan/disks.nix
@@ -1,40 +1,40 @@
 {
   fileSystems."/" = {
-    device = "/dev/nvme0n1p3";
+    device = "/dev/disk/by-uuid/637c6913-61bb-4a02-ac38-7d386153ca5d";
     fsType = "btrfs";
     options = ["subvol=root" "noatime" "compress=zstd" "ssd"];
   };
 
   fileSystems."/home" = {
-    device = "/dev/nvme0n1p3";
+    device = "/dev/disk/by-uuid/637c6913-61bb-4a02-ac38-7d386153ca5d";
     fsType = "btrfs";
     options = ["subvol=home" "noatime" "compress=zstd" "ssd"];
   };
 
   fileSystems."/nix" = {
-    device = "/dev/nvme0n1p3";
+    device = "/dev/disk/by-uuid/637c6913-61bb-4a02-ac38-7d386153ca5d";
     fsType = "btrfs";
     options = ["subvol=nix" "noatime" "compress=zstd" "ssd"];
   };
 
   fileSystems."/var" = {
-    device = "/dev/nvme0n1p3";
+    device = "/dev/disk/by-uuid/637c6913-61bb-4a02-ac38-7d386153ca5d";
     fsType = "btrfs";
     options = ["subvol=var" "noatime" "compress=zstd" "ssd"];
   };
 
   fileSystems."/tmp" = {
-    device = "/dev/nvme0n1p3";
+    device = "/dev/disk/by-uuid/637c6913-61bb-4a02-ac38-7d386153ca5d";
     fsType = "btrfs";
     options = ["subvol=tmp" "noatime" "compress=zstd" "ssd"];
   };
 
   fileSystems."/boot/efi" = {
-    device = "/dev/nvme0n1p1";
+    device = "/dev/disk/by-uuid/10BB-82D1";
     fsType = "vfat";
   };
 
   swapDevices = [
-    {device = "/dev/nvme0n1p2";}
+    {device = "/dev/disk/by-uuid/88668115-cf94-435e-b0d3-80b576abe625";}
   ];
 }
