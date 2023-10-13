3193598e4ab94f5ee77df4f15046677adaefadb5
merrinx <gako358@outlook.com>
2023-10-13 19:22:58 +0200

    update udev rules for zsa

--- hosts/users/terangreal/default.nix
+++ hosts/users/terangreal/default.nix
@@ -16,7 +16,7 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["wheel" "networkmanager" "docker" "libvirtd" "video" "audio"];
+      extraGroups = ["wheel" "networkmanager" "docker" "libvirtd" "video" "audio" "plugdev"];
     };
   };
   # ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-A-1 --mode 2560x1440 --pos 2560x0 --rotate normal
--- hosts/users/tuathaan/default.nix
+++ hosts/users/tuathaan/default.nix
@@ -16,7 +16,7 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = ["wheel" "networkmanager" "docker" "video" "audio"];
+      extraGroups = ["wheel" "networkmanager" "docker" "video" "audio" "plugdev"];
     };
   };
 
