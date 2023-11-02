5ad4e19de9706b830100f621f547bd994547b5d7
merrinx <gako358@outlook.com>
2023-11-02 11:34:02 +0100

    fix xrandr setting for work

--- hosts/users/tuathaan/default.nix
+++ hosts/users/tuathaan/default.nix
@@ -28,6 +28,7 @@
         # xrandr --setprovideroutputsource 2 0 This is after run xrandr --listproviders to identify the provider
         sessionCommands = ''
           ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
+          ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-7 --primary --mode 5120x1440 --pos 1920x0 --rotate normal
         '';
       };
     };
