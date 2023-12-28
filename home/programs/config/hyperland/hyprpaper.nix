eb8e3dcc920be0f15580f173008dddc7d1aa1b51
merrinx <gako358@outlook.com>
2023-12-28 23:55:21 +0100

    Add citrix and update wallpaper for desktop

--- home/programs/config/hyperland/hyprpaper.nix
+++ home/programs/config/hyperland/hyprpaper.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  wallpaper = "/home/merrinx/Sources/archive/images/wallpapers/xmastux.jpg";
+  wallpaper = "/home/merrinx/Sources/archive/images/wallpapers/xmas-girl.png";
 in {
   home.packages = with pkgs; [
     hyprpaper
