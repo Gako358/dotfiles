cd9f8fe8ea006b5882b709cab17482f45abc00e0
merrinx <gako358@outlook.com>
2024-02-22 14:44:19 +0100

    Automatic Update

--- hosts/users/tuathaan/default.nix
+++ hosts/users/tuathaan/default.nix
@@ -1,6 +1,7 @@
-{ pkgs
-, lib
-, ...
+{
+  pkgs,
+  lib,
+  ...
 }: {
   imports = [
     ./hardware-configuration.nix
@@ -13,20 +14,19 @@
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
-      extraGroups = [ "wheel" "networkmanager" "docker" "video" "audio" "plugdev" ];
+      extraGroups = ["wheel" "networkmanager" "docker" "video" "audio" "plugdev"];
     };
   };
 
   services = {
     dbus.enable = true;
     xserver = {
-      videoDrivers = [ "intel" ];
+      videoDrivers = ["intel" "displaylink"];
       displayManager = {
         # xrandr --setprovideroutputsource 2 0 This is after run xrandr --listproviders to identify the provider
         sessionCommands = ''
           ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
-          ${pkgs.xorg.xrandr}/bin/xrandr --output DP-7 --primary --mode 5120x1440 --pos 1920x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
-        '';
+          ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-8 --mode 2560x1440 --pos 1920x0 --rotate normal'';
       };
     };
   };
