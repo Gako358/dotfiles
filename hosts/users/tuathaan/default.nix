7ae9e4fba2d71f74cf398b98bb1e11fb73a76d7d
merrinx <gako358@outlook.com>
2024-02-23 07:51:17 +0100

    Fix laptop dp settings

--- hosts/users/tuathaan/default.nix
+++ hosts/users/tuathaan/default.nix
@@ -26,7 +26,7 @@
         # xrandr --setprovideroutputsource 2 0 This is after run xrandr --listproviders to identify the provider
         sessionCommands = ''
           ${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
-          ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-8 --mode 2560x1440 --pos 1920x0 --rotate normal'';
+          ${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-9 --mode 2560x1440 --pos 1920x0 --rotate normal'';
       };
     };
   };
