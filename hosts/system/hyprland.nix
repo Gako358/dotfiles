962013f4e680891e360f04e11c4f656ea9f86d6f
merrinx <gako358@outlook.com>
2024-04-22 15:03:09 +0200

    Automatic Update

--- hosts/system/hyprland.nix
+++ hosts/system/hyprland.nix
@@ -44,7 +44,7 @@ then {
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-wlr
-      pkgs.xdg-desktop-portal-hyprland
+      # pkgs.xdg-desktop-portal-hyprland
     ];
   };
 
