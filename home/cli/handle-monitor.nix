873c136d49efa0d5de9e280bcac8ae59cfe55a4e
merrinx <gako358@outlook.com>
2024-06-18 20:07:05 +0200

    update nvim flake input
    nvim trouble updated
    nvim metals updated

--- home/cli/handle-monitor.nix
+++ home/cli/handle-monitor.nix
@@ -6,9 +6,11 @@ in
       case $1 in monitoradded*)
         hyprctl dispatch moveworkspacetomonitor "1 1"
         hyprctl dispatch moveworkspacetomonitor "2 1"
+        hyprctl dispatch moveworkspacetomonitor "4 1"
+        hyprctl dispatch moveworkspacetomonitor "7 1"
+        hyprctl dispatch moveworkspacetomonitor "9 1"
       esac
     }
-    echo "XDG_RUNTIME_DIR is: $XDG_RUNTIME_DIR"
 
     ${socat} - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
   ''
--- home/programs/config/hyprland/monitors.nix
+++ home/programs/config/hyprland/monitors.nix
@@ -9,6 +9,7 @@ then {
 else {
   wayland.windowManager.hyprland.extraConfig = ''
     monitor=eDP-1,1920x1080,2560x0,1,bitdepth,10
+    monitor=DP-8,2560x1440,4480x0,1,bitdepth,10
     monitor=DP-9,2560x1440,0x0,1,bitdepth,10
     monitor=,highrr,auto,1
   '';
