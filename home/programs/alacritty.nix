7b92f17e735e6c7ebbccee8abe8e255b684dcb2c
merrinx <gako358@outlook.com>
2024-08-07 21:15:51 +0200

    Add font setup and color for alacritty config

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -14,11 +14,85 @@ in {
           background = "#282828";
           foreground = "#c5c8c6";
         };
+        cursor = {
+          text = "#cdcecf";
+          cursor = "#aeafb0";
+        };
+        vi_mode_cursor = {
+          text = "#cdcecf";
+          cursor = "#63cdcf";
+        };
+        search = {
+          matches = {
+            foreground = "#cdcecf";
+            background = "#3c5372";
+          };
+          focused_match = {
+            foreground = "#cdcecf";
+            background = "#81b29a";
+          };
+        };
+        footer_bar = {
+          foreground = "#cdcecf";
+          background = "#29394f";
+        };
+        hints = {
+          start = {
+            foreground = "#cdcecf";
+            background = "#f4a261";
+          };
+          end = {
+            foreground = "#cdcecf";
+            background = "#29394f";
+          };
+        };
+        normal = {
+          black = "#393b44";
+          red = "#c94f6d";
+          green = "#81b29a";
+          yellow = "#dbc074";
+          blue = "#719cd6";
+          magenta = "#9d79d6";
+          cyan = "#63cdcf";
+          white = "#dfdfe0";
+        };
+        bright = {
+          black = "#575860";
+          red = "#d16983";
+          green = "#8ebaa4";
+          yellow = "#e0c989";
+          blue = "#86abdc";
+          magenta = "#baa1e2";
+          cyan = "#7ad5d6";
+          white = "#e4e4e5";
+        };
+        dim = {
+          black = "#30323a";
+          red = "#ab435d";
+          green = "#6e9783";
+          yellow = "#baa363";
+          blue = "#6085b6";
+          magenta = "#8567b6";
+          cyan = "#54aeb0";
+          white = "#bebebe";
+        };
       };
       font = {
         normal = {
-          family = "JetBrainsMono Nerd Font";
+          family = "FiraCode";
           style = "Medium";
+        };
+        bold = {
+          family = "FiraCode";
+          style = "Bold";
+        };
+        italic = {
+          family = "FiraCode";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "FiraCode";
+          style = "Bold Italic";
         };
         size = fontSize;
       };
