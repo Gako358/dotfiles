7c990f4f9fe4ed7368d4eec8601c3d50d820ef52
merrinx <gako358@outlook.com>
2024-08-08 18:05:14 +0200

    Feat(LUKS) added encryption to root and swap
    - Updated hardware config, setting up LUKS on root and swap partitions
    - Updated install script for desktop, using LUKS

--- scripts/setup_terangreal.sh
+++ scripts/setup_terangreal.sh
@@ -2,57 +2,72 @@
 
 DEVICE1_NAME="/dev/nvme0n1"
 DEVICE2_NAME="/dev/nvme1n1"
+CRYPTROOT_NAME="cryptroot"
+CRYPTSWAP_NAME="cryptswap"
 
 function create_partitions {
-  # Create partitions for DEVICE1_NAME
-  parted --script -a optimal "${DEVICE1_NAME}" mklabel gpt
-  parted --script -a optimal "${DEVICE1_NAME}" mkpart efi 0% 512MiB
-  parted --script -a optimal "${DEVICE1_NAME}" mkpart swap 512MiB 19GiB
-  parted --script -a optimal "${DEVICE1_NAME}" mkpart nixos 19GiB 100%
-  parted --script -a optimal "${DEVICE1_NAME}" set 1 boot on
+    # Create partitions for DEVICE1_NAME
+    parted --script -a optimal "${DEVICE1_NAME}" mklabel gpt
+    parted --script -a optimal "${DEVICE1_NAME}" mkpart efi 0% 512MiB
+    parted --script -a optimal "${DEVICE1_NAME}" mkpart swap 512MiB 19GiB
+    parted --script -a optimal "${DEVICE1_NAME}" mkpart nixos 19GiB 100%
+    parted --script -a optimal "${DEVICE1_NAME}" set 1 boot on
 
-  # Create partitions for DEVICE2_NAME
-  parted --script -a optimal "${DEVICE2_NAME}" mklabel gpt
-  parted --script -a optimal "${DEVICE2_NAME}" mkpart nix 0% 100%
+    # Create partitions for DEVICE2_NAME
+    parted --script -a optimal "${DEVICE2_NAME}" mklabel gpt
+    parted --script -a optimal "${DEVICE2_NAME}" mkpart nix 0% 100%
 
-  clear
-  echo "Partitions created"
+    clear
+    echo "Partitions created"
 
-  # Print table
-  lsblk
+    # Print table
+    lsblk
+}
+
+function setup_luks {
+    # Encrypt the root partition
+    cryptsetup --verify-passphrase -v luksFormat "${DEVICE1_NAME}p3"
+    cryptsetup open "${DEVICE1_NAME}p3" "${CRYPTROOT_NAME}"
+
+    # Encrypt the swap partition
+    cryptsetup --verify-passphrase -v luksFormat "${DEVICE1_NAME}p2"
+    cryptsetup open "${DEVICE1_NAME}p2" "${CRYPTSWAP_NAME}"
+
+    echo "LUKS setup completed"
 }
 
 function mount_partitions {
-  # Format Partitions
-  mkfs.fat -F32 -n EFI "${DEVICE1_NAME}p1"
-  mkswap -L SWAP "${DEVICE1_NAME}p2"
-  mkfs.btrfs -L NIXOS -f "${DEVICE1_NAME}p3"
-  mkfs.btrfs -L NIX -f "${DEVICE2_NAME}p1"
+    # Format Partitions
+    mkfs.fat -F32 -n EFI "${DEVICE1_NAME}p1"
+    mkswap -L SWAP "/dev/mapper/${CRYPTSWAP_NAME}"
+    mkfs.btrfs -L NIXOS -f "/dev/mapper/${CRYPTROOT_NAME}"
+    mkfs.btrfs -L NIX -f "${DEVICE2_NAME}p1"
 
-  echo "Partitions formatted"
+    echo "Partitions formatted"
 
-  # Mount partitions
-  mount "${DEVICE1_NAME}p3" /mnt
+    # Mount partitions
+    mount "/dev/mapper/${CRYPTROOT_NAME}" /mnt
 
-  btrfs subvolume create /mnt/root
-  btrfs subvolume create /mnt/home
-  btrfs subvolume create /mnt/tmp
+    btrfs subvolume create /mnt/root
+    btrfs subvolume create /mnt/home
+    btrfs subvolume create /mnt/tmp
 
-  umount /mnt
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=root "${DEVICE1_NAME}p3" /mnt
+    umount /mnt
+    mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=root "/dev/mapper/${CRYPTROOT_NAME}" /mnt
 
-  mkdir -p /mnt/{boot/efi,home,tmp,nix}
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=home "${DEVICE1_NAME}p3" /mnt/home
-  mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=tmp "${DEVICE1_NAME}p3" /mnt/tmp
-  mount -o noatime,compress=zstd,ssd,space_cache=v2 "${DEVICE2_NAME}p1" /mnt/nix
-  mount "${DEVICE1_NAME}p1" /mnt/boot/efi
-  swapon "${DEVICE1_NAME}p2"
+    mkdir -p /mnt/{boot/efi,home,tmp,nix}
+    mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=home "/dev/mapper/${CRYPTROOT_NAME}" /mnt/home
+    mount -o noatime,compress=zstd,ssd,space_cache=v2,subvol=tmp "/dev/mapper/${CRYPTROOT_NAME}" /mnt/tmp
+    mount -o noatime,compress=zstd,ssd,space_cache=v2 "${DEVICE2_NAME}p1" /mnt/nix
+    mount "${DEVICE1_NAME}p1" /mnt/boot/efi
+    swapon "/dev/mapper/${CRYPTSWAP_NAME}"
 
-  clear
-  df -Th
-  free -h
-  echo "Run: nixos-install --flake .#terangreal"
+    clear
+    df -Th
+    free -h
+    echo "Run: nixos-install --flake .#terangreal"
 }
 
 create_partitions
+setup_luks
 mount_partitions
