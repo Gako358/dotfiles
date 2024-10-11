0abd9dc28816ad61e129b1f14c3c38ac0e6c7f65
Knut Øien <gako358@outlook.com>
2024-10-11 10:39:39 +0200

    Update desktop.nix

--- hosts/system/desktop.nix
+++ hosts/system/desktop.nix
@@ -1,62 +1,64 @@
-{
-  pkgs,
-  specialArgs,
-  ...
-}: if specialArgs.hidpi then {
-  environment.systemPackages = with pkgs; [
-        morewaita-icon-theme
-        qogir-icon-theme
-        gnome-extension-manager
-        wl-clipboard
+{ pkgs, specialArgs, ... }:
 
-	inputs.scramgit.defaultPackage.${pkgs.system}
-	inputs.nvimFlake.defaultPackage.${pkgs.system}
-      ];
+let
+  commonPackages = with pkgs; [
+    morewaita-icon-theme
+    qogir-icon-theme
+    gnome-extension-manager
+    wl-clipboard
 
-      gnome.excludePackages = with pkgs; [
-        # gnome-text-editor
-        atomix # puzzle game
-        cheese # webcam tool
-        epiphany # web browser
-        evince # document viewer
-        gedit
-        geary # email reader
-        gnome-characters
-        gnome-connections
-        gnome-console
-        gnome-contacts
-        gnome-font-viewer
-        gnome-initial-setup
-        gnome-maps
-        gnome-music
-        gnome-photos
-        gnome-shell-extensions
-        gnome-tour
-        iagno # go game
-        snapshot
-        tali # poker game
-        totem # video player
-        hitori # sudoku game
-        yelp # Help view
-      ];
-    };
+    inputs.scramgit.defaultPackage.${pkgs.system}
+    inputs.nvimFlake.defaultPackage.${pkgs.system}
+  ];
+
+  gnomeExcludePackages = with pkgs; [
+    # gnome-text-editor
+    atomix  # puzzle game
+    cheese  # webcam tool
+    epiphany  # web browser
+    evince  # document viewer
+    gedit
+    geary  # email reader
+    gnome-characters
+    gnome-connections
+    gnome-console
+    gnome-contacts
+    gnome-font-viewer
+    gnome-initial-setup
+    gnome-maps
+    gnome-music
+    gnome-photos
+    gnome-shell-extensions
+    gnome-tour
+    iagno  # go game
+    snapshot
+    tali  # poker game
+    totem  # video player
+    hitori  # sudoku game
+    yelp  # Help view
+  ];
+in
+if specialArgs.hidpi then {
+  environment.systemPackages = commonPackages;
+
+  gnome.excludePackages = gnomeExcludePackages;
 
   services.xserver = {
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+} else {
+  services.greetd = let
+    session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+      user = "merrinx";
     };
-    else {
-      services.greetd = let
-        session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
-          user = "merrinx";
-        };
-      in {
-        enable = true;
-        restart = true;
-        settings = {
-          terminal.vt = 1;
-          default_session = session;
-        };
-      };
-    }
+  in {
+    enable = true;
+    restart = true;
+    settings = {
+      terminal.vt = 1;
+      default_session = session;
+    };
+  };
+}
