648f87953a6c584236bd8c9ee26c1225548ac574
merrinx <gako358@outlook.com>
2024-10-30 23:03:08 +0100

    Fix hyprland blur effects

--- home/programs/config/hyprland/hyprland.nix
+++ home/programs/config/hyprland/hyprland.nix
@@ -28,22 +28,7 @@ in {
     };
     decoration = {
       rounding = 16;
-      blur = {
-        enabled = true;
-        brightness = 1.0;
-        contrast = 1.0;
-        noise = 0.01;
-
-        vibrancy = 0.2;
-        vibrancy_darkness = 0.5;
-
-        passes = 4;
-        size = 7;
-
-        popups = true;
-        popups_ignorealpha = 0.2;
-      };
-
+      blur.enabled = true;
       drop_shadow = true;
       shadow_ignore_window = true;
       shadow_offset = "0 15";
