beeba9742fa8fcce64bd0610d8abb7cbdaf8048e
merrinx <gako358@outlook.com>
2025-02-10 19:11:58 +0100

    Fix opacity hyprland

--- home/programs/config/hyprland/hyprpaper.nix
+++ home/programs/config/hyprland/hyprpaper.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  wallpaper = "/home/merrinx/Sources/archive/images/wallpapers/winter.png";
+  wallpaper = "/home/merrinx/Sources/archive/images/wallpapers/dark_moon.jpg";
 in {
   home.packages = with pkgs; [
     hyprpaper
