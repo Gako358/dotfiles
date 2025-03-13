e8ddbd9bf4620b3e9c7d537ee0d8e5704e774644
merrinx <gako358@outlook.com>
2025-03-13 09:06:53 +0100

    Fix action using own cachix

--- modules/programs/wezterm.nix
+++ modules/programs/wezterm.nix
@@ -143,10 +143,9 @@
       config.tab_and_split_indices_are_zero_based = false
       local LEFT_END = utf8.char(0xE0B6)
       local RIGHT_END = utf8.char(0xE0B4)
+      local bg_color = "#1a1a1a"
       local active_tab_bg_color = "#FFA066"
-      local inactive_tab_text_color = "#719cd6"
-      local active_tab_fg_color = "#1a1a1a"
-      local inactive_tab_bg_color = "#1a1a1a"
+      local inactive_text_color = "#719cd6"
 
       function tab_title(tab_info)
         local title = tab_info.tab_title
@@ -164,39 +163,35 @@
         function(tab, tabs, panes, config, hover, max_width)
           local title = tab_title(tab)
           title = wezterm.truncate_right(title, max_width - 2)
-          local main_bg_color = "#1a1a1a"
-          local background = "#1a1a1a"
-          local tab_icon_inactive = "#719cd6"
-          local tab_icon_inactive_icon = wezterm.nerdfonts.md_ghost_off_outline
           local tab_icon_active_icon = wezterm.nerdfonts.md_ghost
-          local icon_text = ""
-          local tab_icon_color = ""
-          local tab_text_color = ""
-          local tab_background_color = "#1a1a1a"
+          local tab_icon_inactive_icon = wezterm.nerdfonts.md_ghost_off_outline
+          local icon_text = " "
+          local tab_icon_color = " "
+          local tab_text_color = " "
+          local tab_background_color = bg_color
 
           if tab.is_active then
-            tab_icon_color = active_tab_fg_color
-            tab_text_color = active_tab_fg_color
+            tab_icon_color = bg_color
+            tab_text_color = bg_color
             tab_background_color = active_tab_bg_color
             icon_text = tab_icon_active_icon
           else
-            tab_icon_color = tab_icon_inactive
-            tab_text_color = inactive_tab_text_color
+            tab_icon_color = inactive_text_color
+            tab_text_color = inactive_text_color
             icon_text = tab_icon_inactive_icon
-            tab_background_color = inactive_tab_bg_color
           end
 
           return {
-            { Background = { Color = main_bg_color } },
+            { Background = { Color = bg_color } },
             { Foreground = { Color = tab_background_color } },
             { Text = LEFT_END },
             { Background = { Color = tab_background_color } },
             { Foreground = { Color = tab_icon_color } },
-            { Text = " " .. icon_text .. " " },
+            { Text = "  " .. icon_text .. "  " },
             { Background = { Color = tab_background_color } },
             { Foreground = { Color = tab_text_color } },
-            { Text = title .. "  " },
-            { Background = { Color = background } },
+            { Text = title .. "   " },
+            { Background = { Color = bg_color } },
             { Foreground = { Color = tab_background_color } },
             { Text = RIGHT_END },
           }
