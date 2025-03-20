9feed6e0e25ec7c62429b45c4b2af3af5b97f924
merrinx <gako358@outlook.com>
2025-03-20 21:59:53 +0100

    Cleanup emacs config

--- modules/programs/config/emacs/default.nix
+++ modules/programs/config/emacs/default.nix
@@ -65,6 +65,31 @@ in
     package = pkgs.emacs30;
     extraPackages = epkgs:
       with epkgs; [
+        # Appearande
+        dashboard # A startup screen extracted from Spacemacs
+        kaolin-themes # A low contrast color theme for Emacs
+        nerd-icons # Nerd icons for Emacs
+        nerd-icons-completion # Nerd icons for completion
+        nerd-icons-corfu # Nerd icons for corfu
+        all-the-icons-completion
+        rainbow-delimiters # Highlight delimiters such as parentheses, brackets or braces according to their depth
+        rainbow-mode # Colorize color names in buffers
+        all-the-icons # A package for inserting developer icons
+        all-the-icons-ivy-rich # More friendly display transformer for ivy
+
+        # Completion
+        orderless # Space-separated matching components.
+        vertico # Vertical interactive completion UI.
+        marginalia # Annotations for completion candidates.
+        embark # Context-sensitive actions.
+        embark-consult # Integration between embark and consult.
+        consult # Commands compatible with completing-read.
+        corfu # Completion Overlay Region Function.
+        cape # Completion At Point Extensions.
+        yasnippet # Template system for Emacs.
+        wgrep # Writable grep buffer.
+        copilot # AI code completion.
+
         # Evil
         evil # Extensible vi layer for Emacs
         evil-collection # A set of keybindings for evil-mode
@@ -80,18 +105,15 @@ in
         undo-fu # Undo framework for Emacs
         undo-fu-session # Persistent undo for Emacs
 
-        # Completion
-        orderless # Space-separated matching components.
-        vertico # Vertical interactive completion UI.
-        marginalia # Annotations for completion candidates.
-        embark # Context-sensitive actions.
-        embark-consult # Integration between embark and consult.
-        consult # Commands compatible with completing-read.
-        corfu # Completion Overlay Region Function.
-        cape # Completion At Point Extensions.
-        yasnippet # Template system for Emacs.
-        wgrep # Writable grep buffer.
-        copilot # AI code completion.
+        # Env
+        envrc # .envrc support for Emacs
+
+        # Edit
+        apheleia # A universal formatter interface
+        markdown-mode # Major mode for editing Markdown files
+
+        # Filetree
+        dirvish # Directory viewer for Emacs
 
         # General
         dash # A modern list library for Emacs
@@ -100,6 +122,21 @@ in
         general # Provides a more convenient way to define keybindings
         ligature # Ligature support for Emacs
         s # The long lost Emacs string manipulation library
+        password-store # Emacs interface for pass, the standard Unix password manager
+
+        # grammars
+        treesit-grammars.with-all-grammars # Tree-sitter grammars
+
+        # Programming language packages.
+        nix-ts-mode # Major mode for editing Nix files
+        scala-ts-mode # Scala development environment
+        web-mode # Major mode for editing web templates
+        yaml-mode # Major mode for editing YAML files
+        vue-ts-mode # Major mode for editing Vue3 files
+
+        # LSP
+        eglot-booster # Eglot booster
+        eldoc-box # Display function signatures at point
 
         # Org
         org # For keeping notes, maintaining TODO lists, and project planning
@@ -110,54 +147,20 @@ in
         org-roam # A note-taking tool based on the principles of networked thought
         org-roam-ui # A graphical user interface for org-roam
 
-        # Filetree
-        dirvish # Directory viewer for Emacs
-
         # Project
         counsel # Various completion functions using Ivy
         counsel-projectile # Ivy integration for Projectile
         eat # Emacs All
         projectile # Project Interaction Library for Emacs
 
-        # Env
-        envrc # .envrc support for Emacs
+        # Terminal
+        vterm # A terminal emulator for Emacs
 
-        # Git
+        # Version
         blamer # Show git blame information in the fringe
         git-gutter # Show git diff in the fringe
         git-gutter-fringe # Fringe version of git-gutter.el
         magit # A Git porcelain inside Emacs
-
-        # Theme
-        dashboard # A startup screen extracted from Spacemacs
-        kaolin-themes # A low contrast color theme for Emacs
-        nerd-icons # Nerd icons for Emacs
-        all-the-icons-completion
-        rainbow-delimiters # Highlight delimiters such as parentheses, brackets or braces according to their depth
-        rainbow-mode # Colorize color names in buffers
-        all-the-icons # A package for inserting developer icons
-        all-the-icons-ivy-rich # More friendly display transformer for ivy
-
-        # Edit
-        apheleia # A universal formatter interface
-        markdown-mode # Major mode for editing Markdown files
-
-        # grammars
-        treesit-grammars.with-all-grammars # Tree-sitter grammars
-
-        # LSP
-        eglot-booster # Eglot booster
-        eldoc-box # Display function signatures at point
-
-        # Programming language packages.
-        nix-ts-mode # Major mode for editing Nix files
-        python # Python development environment
-        python-test # Python test environment
-        rustic # Rust development environment
-        scala-ts-mode # Scala development environment
-        web-mode # Major mode for editing web templates
-        yaml-mode # Major mode for editing YAML files
-        vue-ts-mode # Major mode for editing Vue3 files
       ];
     extraConfig = builtins.readFile ./init.el;
   };
