79986bb537e2cc12b23e1db2f5bf2fd3c9a5531f
merrinx <gako358@outlook.com>
2025-03-20 08:21:47 +0100

    Remove emacs config

--- modules/programs/config/emacs/init.el
+++
@@ -1,7 +0,0 @@
-;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
-
-(org-babel-load-file (expand-file-name "/home/merrinx/Sources/dotfiles/home/programs/config/emacs/config.org"))
-
-;;; init.el ends here
