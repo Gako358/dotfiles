5e264bf35f28a5cb2f9e1877ec00cc39bf1b58b1
merrinx <gako358@outlook.com>
2025-03-26 12:41:09 +0100

    Enhance CAPE configuration with capf-accept-all
    fix(scramgit): comment out scramgit service due to environment issues
    
    The CAPE configuration now uses ~cape-capf-accept-all~ for better completion at point functions. The scramgit service has been commented out temporarily to address environment issues with Python and shell execution.

--- system/services/scramgit.nix
+++ system/services/scramgit.nix
@@ -4,13 +4,14 @@
     inputs.scramgit.defaultPackage.${pkgs.system}
   ];
 
-  systemd.user.services.scramgit = {
-    description = "Run scramgit to update repositories";
-    wantedBy = [ "default.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${inputs.scramgit.defaultPackage.${pkgs.system}}/bin/scramgit";
-    };
-  };
+  # TODO: Need to fix env for python, and shell to run scram
+  # systemd.user.services.scramgit = {
+  #   description = "Run scramgit to update repositories";
+  #   wantedBy = [ "default.target" ];
+  #   after = [ "graphical-session.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "${inputs.scramgit.defaultPackage.${pkgs.system}}/bin/scramgit";
+  #   };
+  # };
 }
