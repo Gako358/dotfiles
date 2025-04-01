cc1a4c5154dae2400373ed38ee96379674a5720a
merrinx <gako358@outlook.com>
2025-04-01 21:33:14 +0200

    Enhance browser configuration with custom search engines
    Add multiple search engines with custom completion functions and shortcuts.
    Improve buffer configuration with smooth scrolling and default search engine.
    Update keybindings and cleanup unused code.

--- modules/programs/config/nyxt/default.nix
+++ modules/programs/config/nyxt/default.nix
@@ -11,8 +11,7 @@
   ];
 
   xdg.configFile."nyxt/config.lisp".text = /*lisp*/''
-    ;; This is a sample configuration file for Nyxt.
-    ;; It demonstrates how to configure Nyxt using the Common Lisp language.
+    ;; Define browser configuration
     (define-configuration browser
       ((theme
         (make-instance 'theme:theme
@@ -21,67 +20,105 @@
                        :contrast-text-color "#303035"    ;; bivrost2 - Alternative dark color
                        :primary-color "#719cd6"          ;; bivrost9 - Blue color
                        :secondary-color "#303035"        ;; bivrost2 - Alternative dark color
-
                        :action-color "#303035"           ;; bivrost2  - Alternative dark background
                        :highlight-color "#B48EAD"        ;; bivrost16 - Purple color
                        :success-color "#A3BE8C"          ;; bivrost15 - Green color
                        :warning-color "#D08770"          ;; bivrost13 - Light orange color
                        :codeblock-color "#303035"        ;; bivrost2  - Alternative dark color
                        :error-color "#BF616A")           ;; bivrost12 - Red color
-        :doc "Custom Bivrost Dark Theme based on Emacs colors")))
+        :doc "Custom Bivrost Dark Theme based on Emacs colors"))
+       (search-engines
+        (list
+
+         (make-instance 'search-engine
+                        :name "QuickDocs"
+                        :shortcut "quickdocs"
+                        :search-url "http://quickdocs.org/search?q=~a"
+                        :fallback-url "http://quickdocs.org/")
+         (make-instance 'search-engine
+                        :name "Wikipedia"
+                        :shortcut "wiki"
+                        :search-url "https://en.wikipedia.org/w/index.php?search=~a"
+                        :fallback-url "https://en.wikipedia.org/"
+                        :completion-function
+                        (make-search-completion-function
+                         :base-url "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=~a"
+                         :processing-function (lambda (response)
+                                              (second (njson:decode response)))))
+         (make-instance 'search-engine
+                        :name "Wiktionary"
+                        :shortcut "define"
+                        :search-url "https://en.wiktionary.org/w/index.php?search=~a"
+                        :fallback-url "https://en.wiktionary.org/"
+                        :completion-function
+                        (make-search-completion-function
+                         :base-url "https://en.wiktionary.org/w/api.php?action=opensearch&format=json&search=~a"
+                         :processing-function (lambda (response)
+                                              (second (njson:decode response)))))
+         (make-instance 'search-engine
+                        :name "NixOS Packages"
+                        :shortcut "nix"
+                        :search-url "https://search.nixos.org/packages?query=~a"
+                        :fallback-url "https://search.nixos.org")
+         (make-instance 'search-engine
+                        :name "Reddit"
+                        :shortcut "reddit"
+                        :search-url "https://www.reddit.com/search/?q=~a"
+                        :fallback-url "https://www.reddit.com/search")
+         (make-instance 'search-engine
+                        :name "DuckDuckGo"
+                        :shortcut "duck"
+                        :search-url "https://duckduckgo.com/?q=~a"
+                        :fallback-url "https://duckduckgo.com"
+                        :completion-function
+                        (make-search-completion-function
+                         :base-url "https://duckduckgo.com/ac/?q=~a"
+                         :processing-function (lambda (response)
+                                              (mapcar (lambda (x) (gethash "phrase" x))
+                                                     (njson:decode response)))))
+         (make-instance 'search-engine
+                        :name "YouTube"
+                        :shortcut "yt"
+                        :search-url "https://www.youtube.com/results?search_query=~a"
+                        :fallback-url "https://www.youtube.com/"
+                        :completion-function
+                        (make-search-completion-function
+                         :base-url "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&q=~a"
+                         :processing-function (lambda (response)
+                                              (mapcar #'first (second (njson:decode response))))))
+         (make-instance 'search-engine
+                        :name "Google"
+                        :shortcut "google"
+                        :search-url "https://www.google.com/search?q=~a"
+                        :fallback-url "https://www.google.com/"
+                        :completion-function
+                        (make-search-completion-function
+                         :base-url "https://suggestqueries.google.com/complete/search?client=firefox&q=~a"
+                         :processing-function (lambda (response)
+                                              (mapcar #'first (second (njson:decode response)))))))))
 
     ;; Reroute bookmarks to a static file
     (defmethod files:resolve ((profile nyxt:nyxt-profile) (file nyxt/mode/bookmark:bookmarks-file))
       "Reroute the bookmarks to the config directory."
       #p"~/.config/nyxt/bookmarks.lisp")
 
+    ;; Buffer configuration
     (define-configuration buffer
-      ((default-modes
-         (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))
+      ((smooth-scrolling t)
+       (default-search-engine "duck")  ;; Set DuckDuckGo as default
+       (default-modes (append '(nyxt/mode/vi:vi-normal-mode) %slot-value%))
        (external-editor-program '("emacsclient"))
        (override-map (let ((map (make-keymap "override-map")))
-                    (define-key map "M-n" 'new)
-                    (define-key map "M-f" 'switch-buffer)
-                    (define-key map "M-b" 'list-bookmarks)
-                    (define-key map "M-x" 'delete-buffer)
-                    map))))
+                       (define-key map "M-f" 'switch-buffer)
+                       (define-key map "M-o" 'set-url-from-bookmark)
+                       (define-key map "M-x" 'delete-buffer)
+                       (define-key map "M-q" 'delete-all-buffers)
+                       (define-key map "\"" 'search-buffers)
+                       map))))
 
-
-    (defvar *duckduckgo-keywords*
-      '(:theme :dark
-        :help-improve-duckduckgo nil
-        :homepage-privacy-tips nil
-        :privacy-newsletter nil
-        :newsletter-reminders nil
-        :install-reminders nil
-        :install-duckduckgo nil
-        :units-of-measure :metric
-        :keyboard-shortcuts t
-        :advertisements nil
-        :open-in-new-tab nil
-        :infinite-scroll t
-        :safe-search :off
-        :font-size :medium
-        :header-behavior :on-fixed
-        :font :helvetica
-        :background-color "242424"
-        :center-alignment t))
-
-    (defvar *my-search-engines*
-      (list
-       '("google" "https://google.com/search?q=~a" "https://google.com")
-       '("wikipedia" "https://en.wikipedia.org/w/index.php?search=~a" "https://en.wikipedia.org")
-       '("reddit" "https://www.reddit.com/search/?q=~a" "https://www.reddit.com")
-       '("nixpackages" "https://search.nixos.org/packages?query=~a" "https://search.nixos.org/packages")
-       '("duckduckgo" "https://duckduckgo.com/?q=~a" "https://duckduckgo.com")))
-
-    (define-configuration context-buffer
-      "Configure the search engines."
-      ((search-engines
-        (append
-         (mapcar (lambda (engine) (apply 'make-search-engine engine))
-                *my-search-engines*)
-         %slot-default%))))
+    ;; Prompt buffer configuration
+    (define-configuration prompt-buffer
+      ((default-modes (append '(nyxt/mode/vi:vi-insert-mode) %slot-default%))))
   '';
 
   xdg.configFile."nyxt/bookmarks.lisp".text = /*lisp*/''
