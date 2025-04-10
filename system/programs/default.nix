50137230a1deb19104ef5ea10de5592819051b7a
merrinx <gako.footwork856@passinbox.com>
2025-04-10 07:39:05 +0200

    Restructure environment configuration
    Refactored the NixOS environment configuration to improve readability and maintainability. Consolidated `environment.systemPackages` and `environment.persistence` definitions. Adjusted directory and file persistence settings for user `merrinx`.

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -13,50 +13,51 @@ with lib; {
     ./xdg.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    wget
-    curl
-    git
-  ];
-  environment.persistence."/persist" = {
-    enable = true;
-    hideMounts = true;
-    directories = [
-      "/var/log"
-      "/var/lib"
-      "/etc/ssh"
-      "/etc/NetworkManager/system-connections"
+  environment = {
+    systemPackages = with pkgs; [
+      wget
+      curl
+      git
     ];
-    files = [
-      "/etc/machine-id"
-    ];
-    users.merrinx = {
+    persistence."/persist" = {
+      hideMounts = true;
       directories = [
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".nixops"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        { directory = ".pulumi"; mode = "0700"; }
-
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Documents"
-
-        ".local/share/direnv"
-
-        ".cargo"
-        ".m2"
-        ".npm"
-
-        ".config/discord"
-        ".config/protonmail"
-        ".config/Slack"
-        ".config/spotify"
+        "/var/log"
+        "/var/lib"
+        "/etc/ssh"
+        "/etc/NetworkManager/system-connections"
       ];
       files = [
-        ".screenrc"
+        "/etc/machine-id"
       ];
+      users.merrinx = {
+        directories = [
+          { directory = ".gnupg"; mode = "0700"; }
+          { directory = ".ssh"; mode = "0700"; }
+          { directory = ".nixops"; mode = "0700"; }
+          { directory = ".local/share/keyrings"; mode = "0700"; }
+          { directory = ".pulumi"; mode = "0700"; }
+
+          "Downloads"
+          "Music"
+          "Pictures"
+          "Documents"
+
+          ".local/share/direnv"
+
+          ".cargo"
+          ".m2"
+          ".npm"
+
+          ".config/discord"
+          ".config/protonmail"
+          ".config/Slack"
+          ".config/spotify"
+        ];
+        files = [
+          ".screenrc"
+        ];
+      };
     };
   };
 }
