0481714ecc2cb0632fa4f5a2efab53ae7fa89631
merrinx <gako.footwork856@passinbox.com>
2025-04-11 12:47:25 +0200

    Remove unused TLS configuration
    Removed unused TLS and STARTTLS settings from the msmtp configuration. These settings were redundant as the service operates on a local host and port without requiring TLS.

--- system/services/msmtp.nix
+++ system/services/msmtp.nix
@@ -7,13 +7,10 @@ in
   programs.msmtp = {
     enable = true;
     accounts.default = {
-      auth = true;
       host = "127.0.0.1";
       port = 1025;
       from = "${cat} ${config.sops.secrets."email_username".path}";
       user = "${cat} ${config.sops.secrets."email_username".path}";
-      tls = true;
-      tls_starttls = true;
       passwordeval = "${cat} ${config.sops.secrets."email_home-passwd".path}";
     };
   };
