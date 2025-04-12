edd3df06057b194133814c24b7c452dbae7cfb54
merrinx <gako.footwork856@passinbox.com>
2025-04-12 16:27:20 +0200

    Finishing up new install

--- system/services/default.nix
+++ system/services/default.nix
@@ -4,12 +4,12 @@
     ./btrbk.nix
     ./dbus.nix
     ./gnome.nix
-    #./msmtp.nix
+    ./msmtp.nix
     ./nfc.nix
     ./openssh.nix
     ./pipwire.nix
     ./scramgit.nix
-    #./secrets.nix
+    ./secrets.nix
     ./wally.nix
   ];
 }
