be29cbbc581d549333107d798f361fb29ba2ca34
merrinx <gako.footwork856@passinbox.com>
2025-04-14 20:49:41 +0200

    Remove unused email-related secrets configuration
    The email-related secrets configuration has been removed as it is no longer required. This simplifies the secrets management setup by eliminating redundant entries.

--- system/services/secrets.nix
+++ system/services/secrets.nix
@@ -3,34 +3,6 @@
     defaultSopsFile = ../../secrets/default.yaml;
     age.keyFile = "/home/merrinx/.config/sops/age/keys.txt";
     validateSopsFiles = false;
-    secrets = {
-      "email_master-passwd" = {
-        mode = "0600";
-        owner = "merrinx";
-        group = "users";
-      };
-      "email_home-passwd" = {
-        mode = "0600";
-        owner = "merrinx";
-        group = "users";
-      };
-      "email_work-passwd" = {
-        mode = "0600";
-        owner = "merrinx";
-        group = "users";
-      };
-      "email_user" = {
-        mode = "0600";
-        owner = "merrinx";
-        group = "users";
-      };
-      "email_username" = {
-        mode = "0600";
-        owner = "merrinx";
-        group = "users";
-      };
-    };
-
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
