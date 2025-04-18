46bca2f183c689e92ec29847cf1b9113d1d1fcaa
merrinx <gako.footwork856@passinbox.com>
2025-04-18 23:50:58 +0200

    Add configuration options for Spotify TUI
    Introduce a new configuration module for Spotify TUI with customizable options. This includes enabling the program, setting themes, icons, and playback settings. The configuration now uses `mkOption` for better integration and flexibility.
    
    BREAKING CHANGE: The configuration structure for Spotify TUI has been refactored. Users need to update their configurations to match the new format.

--- modules/programs/spotify.nix
+++ modules/programs/spotify.nix
@@ -1,44 +1,56 @@
 { config
-, pkgs
+, lib
 , ...
 }:
+with lib;
 let
-  cat = "${pkgs.coreutils}/bin/cat";
+  cfg = config.program.spotify;
 in
 {
-  sops.secrets = {
-    "spotify_id" = { };
-    "spotify_secret" = { };
+
+  options.program.spotify = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable Spotify TUI.";
+    };
   };
 
-  home.persistence."/persist/${config.home.homeDirectory}" = {
-    directories = [
-      ".config/spotify-player"
-      ".cache/spotify-player"
-    ];
-  };
+  config = mkIf cfg.enable {
+    sops.secrets = {
+      "spotify_id" = { };
+      "spotify_secret" = { };
+    };
 
-  programs.spotify-player = {
-    enable = true;
-    settings = {
-      theme = "default";
-      border_type = "Rounded";
-      progress_bar_type = "Line";
-      playback_window_position = "Top";
-      play_icon = "";
-      pause_icon = "";
-      liked_icon = "";
-      copy_command = {
-        command = "wl-copy";
-        args = [ ];
-      };
-      client_id = "${cat} ${config.sops.secrets."spotify_id".path}";
-      client_port = 8888;
-      ap_port = 443;
-      device = {
-        audio_cache = true;
-        normalization = true;
-        volume = 50;
+    home.persistence."/persist/${config.home.homeDirectory}" = {
+      directories = [
+        ".config/spotify-player"
+        ".cache/spotify-player"
+      ];
+    };
+
+    programs.spotify-player = {
+      enable = true;
+      settings = {
+        theme = "default";
+        border_type = "Rounded";
+        progress_bar_type = "Line";
+        playback_window_position = "Top";
+        play_icon = "";
+        pause_icon = "";
+        liked_icon = "";
+        copy_command = {
+          command = "wl-copy";
+          args = [ ];
+        };
+        client_id = "65b708073fc0480ea92a077233ca87bd";
+        client_port = 8888;
+        ap_port = 443;
+        device = {
+          audio_cache = true;
+          normalization = true;
+          volume = 50;
+        };
       };
     };
   };
