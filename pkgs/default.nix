4da449f5bba8011787fa531d2cd94a16805b780b
merrinx <gako.footwork856@passinbox.com>
2025-04-22 18:14:24 +0200

    Cleanup pkgs and config for parts modules

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,7 @@
 { inputs, lib, config, pkgs, system, ... }: {
-  # Use the standard perSystem arguments, including 'pkgs'
   perSystem = { pkgs, lib, config, system, ... }: {
     packages = {
-      # Use the 'pkgs' provided to perSystem
-      pass-wofi = pkgs.callPackage ./pass-wofi {
-        # No overrides needed here if pass-wofi/default.nix defines all deps
-      };
+      pass-wofi = pkgs.callPackage ./pass-wofi { };
     };
   };
 }
