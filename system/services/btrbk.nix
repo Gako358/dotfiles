0e7e48cf46773362f69d1bd625b32d9af271a3fd
merrinx <gako.footwork856@passinbox.com>
2025-04-23 22:12:51 +0200

    Update snapshot retention settings
    Updated the snapshot retention settings for the ~btrbak~ instance:
    - Changed ~snapshot_preserve_min~ to ~3d~.
    - Adjusted ~snapshot_preserve~ to ~7d 4w 3m~ for better retention management.
    - Removed redundant comments for clarity.

--- system/services/btrbk.nix
+++ system/services/btrbk.nix
@@ -1,15 +1,13 @@
 {
   services.btrbk = {
     instances."btrbak" = {
-      # Take snapshots three times a day: 6am, 2pm, and 10pm
       onCalendar = "*-*-* 06,14,22:00:00";
       settings = {
         timestamp_format = "long";
-        # Retention settings
-        snapshot_preserve_min = "3d"; # Keep all snapshots for at least 2 days
-        preserve_day_of_week = "sunday"; # Keep Sunday snapshots longer
-        preserve_hour_of_day = "6"; # Use the 6am snapshot for weekly retention
-        target_preserve = "48h 7d 4w";
+        snapshot_preserve_min = "3d";
+        snapshot_preserve = "7d 4w 3m";
+        preserve_day_of_week = "sunday";
+        preserve_hour_of_day = "6";
         volume."/persist" = {
           snapshot_create = "always";
           subvolume = ".";
