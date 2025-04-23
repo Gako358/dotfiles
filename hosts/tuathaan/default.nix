e9295b5b7b61721bf675e899e771639d56146072
merrinx <gako.footwork856@passinbox.com>
2025-04-23 17:56:49 +0200

    Remove obsolete SSH key TODO

--- hosts/tuathaan/default.nix
+++ hosts/tuathaan/default.nix
@@ -9,7 +9,6 @@
       isNormalUser = true;
       initialHashedPassword = "$6$S/5rsm5aa6ldGB90$uTqLQhPl6edMUy03a.bdzD2qILs28c74j1YcRHYHtFey/O1g7QDkpqRSpQBdA0hGLdimPMVrftjdyRn6dOWis.";
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       extraGroups = [ "wheel" "networkmanager" "docker" "video" "audio" "plugdev" ];
     };
