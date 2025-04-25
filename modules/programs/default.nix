6bf4f36f15ccf59f41fec18c33c2694198a0a8be
merrinx <gako.footwork856@passinbox.com>
2025-04-25 20:10:31 +0200

    Moved shell packages into fish configuration

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -2,25 +2,17 @@ let
   more = { pkgs, ... }: {
     home.packages = with pkgs; [
       acpi # battery info
-      any-nix-shell # fish support for nix shell
       arandr # screen layout manager
       brightnessctl # control screen brightness
       bottom # alternative to htop & ytop
       cacert # ca certificates
       dconf2nix # dconf (gnome) files to nix converter
-      dive # explore docker layers
-      duf # disk usage/free utility
-      eza # a better `ls`
-      fd # "find" for files
       ffmpegthumbnailer # thumbnailer for video files
       headsetcontrol # control logitech headsets
       imagemagick # image manipulation
-      jump # fast directory navigation
       killall # kill processes by name
       libsecret # secret management
       mediainfo # media information
-      ncdu # disk space info (a better du)
-      nitch # minimal system information fetch
       nix-index # locate packages containing certain nixpkgs
       nix-output-monitor # nom: monitor nix commands
       nix-prefetch-git # prefetch git sources
--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -6,10 +6,22 @@ let
   };
 in
 {
-  home.persistence."/persist/${config.home.homeDirectory}" = {
-    directories = [
-      ".local/share/fish"
+  home = {
+    packages = with pkgs; [
+      any-nix-shell # fish support for nix shell
+      dive # explore docker layers
+      duf # disk usage/free utility
+      eza # a better `ls`
+      fd # "find" for files
+      jump # fast directory navigation
+      ncdu # disk space info (a better du)
+      nitch # minimal system information fetch
     ];
+    persistence."/persist/${config.home.homeDirectory}" = {
+      directories = [
+        ".local/share/fish"
+      ];
+    };
   };
   programs.fish = {
     enable = true;
