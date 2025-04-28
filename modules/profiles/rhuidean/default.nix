f196d7c3934860b5f7d84e35af38a1096035397c
merrinx <gako.footwork856@passinbox.com>
2025-04-28 21:01:03 +0200

    Fix monitor setting for VDI

--- modules/profiles/rhuidean/default.nix
+++ modules/profiles/rhuidean/default.nix
@@ -6,23 +6,22 @@
   imports = [ ../../default.nix ];
 
   wayland.windowManager.hyprland.extraConfig = lib.mkIf (osConfig.environment.desktop.windowManager == "hyprland") ''
-    monitor=DP-2,2560x1440,0x0,1
-    monitor=DP-3,2560x1440,2560x0,1
+    monitor=virtual-1,2560x1440,0x0,1
     monitor=,highrr,auto,1
 
-    workspace = 1, monitor:DP-2
-    workspace = 2, monitor:DP-2
-    workspace = 3, monitor:DP-2
-    workspace = 4, monitor:DP-2
-    workspace = 5, monitor:DP-2
-    workspace = 6, monitor:DP-2
-    workspace = 7, monitor:DP-2
-    workspace = 8, monitor:DP-2
-    workspace = 9, monitor:DP-2
+    workspace = 1, monitor:virtual-1
+    workspace = 2, monitor:virtual-1
+    workspace = 3, monitor:virtual-1
+    workspace = 4, monitor:virtual-1
+    workspace = 5, monitor:virtual-1
+    workspace = 6, monitor:virtual-1
+    workspace = 7, monitor:virtual-1
+    workspace = 8, monitor:virtual-1
+    workspace = 9, monitor:virtual-1
   '';
 
   # Home modules to load
-  program.hyprlock.defaultMonitor = "DP-2";
+  program.hyprlock.defaultMonitor = "virtual-1";
 
   service.hypridle = {
     timeout = 3600;
