6c4875669a7787714948d2f47f90b6b87025771c
merrinx <gako.footwork856@passinbox.com>
2025-05-04 10:01:37 +0200

    Mako api changes
    No longer accept int as input, move to using strings

--- modules/services/mako.nix
+++ modules/services/mako.nix
@@ -13,10 +13,10 @@ in
     font = fontType;
     padding = "10,20";
     anchor = "top-right";
-    width = 460;
-    height = 190;
+    width = "460";
+    height = "190";
     borderSize = "1";
-    defaultTimeout = 7300; # 7 sec
+    defaultTimeout = "7300"; # 7 sec
     backgroundColor = "#${config.colorScheme.palette.base00}cc";
     borderColor = "#${config.colorScheme.palette.base0D}cc";
     textColor = "#${config.colorScheme.palette.base05}ff";
