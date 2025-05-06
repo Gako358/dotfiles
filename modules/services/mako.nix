a544e2dbe6155abefdf741646ac0d8273b1af4f1
merrinx <gako.footwork856@passinbox.com>
2025-05-06 08:54:23 +0200

    Fix error in mako configuration
    Nix documentation for mako config was wrong.
    Mako does not use camel case, instead kebab-case in settings

--- modules/services/mako.nix
+++ modules/services/mako.nix
@@ -10,17 +10,17 @@ in
   services.mako = lib.mkIf (osConfig.environment.desktop.windowManager == "hyprland") {
     enable = true;
     settings = {
-      iconPath = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
+      icon-path = "${config.gtk.iconTheme.package}/share/icons/Papirus-Dark";
       font = fontType;
       padding = "10,20";
       anchor = "top-right";
       width = "460";
       height = "190";
-      borderSize = "1";
-      defaultTimeout = "7300"; # 7 sec
-      backgroundColor = "#${config.colorScheme.palette.base00}cc";
-      borderColor = "#${config.colorScheme.palette.base0D}cc";
-      textColor = "#${config.colorScheme.palette.base05}ff";
+      border-size = "1";
+      default-timeout = "7300"; # 7 sec
+      background-color = "#${config.colorScheme.palette.base00}cc";
+      border-color = "#${config.colorScheme.palette.base0D}cc";
+      text-color = "#${config.colorScheme.palette.base05}ff";
       layer = "overlay";
     };
   };
