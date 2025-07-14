3e990a58f37441964896562c4ca71d83fc42ac86
merrinx <gako.footwork856@passinbox.com>
2025-07-14 20:07:59 +0200

    Remove bitdepth from monitor settings - hyprland

--- modules/profiles/tuathaan/default.nix
+++ modules/profiles/tuathaan/default.nix
@@ -11,22 +11,24 @@ in
 {
   imports = [ ../../default.nix ];
 
-  wayland.windowManager.hyprland.extraConfig = lib.mkIf (osConfig.environment.desktop.windowManager == "hyprland") ''
-    monitor=eDP-1,1920x1200,2560x1440,1, bitdepth, 8
-    monitor=DP-8,2560x1440,0x0,1, bitdepth, 8
-    monitor=DP-6,2560x1440,2560x0,1, bitdepth, 8
-    monitor=,highrr,auto,1
+  wayland.windowManager.hyprland.extraConfig =
+    lib.mkIf (osConfig.environment.desktop.windowManager == "hyprland")
+      ''
+        monitor=eDP-1,1920x1200,2560x1440,1
+        monitor=DP-8,2560x1440,0x0,1
+        monitor=DP-6,2560x1440,2560x0,1
+        monitor=,highrr,auto,1
 
-    workspace = 1, monitor:DP-6
-    workspace = 2, monitor:DP-8
-    workspace = 3, monitor:DP-8
-    workspace = 4, monitor:DP-8
-    workspace = 5, monitor:DP-8
-    workspace = 6, monitor:DP-6
-    workspace = 7, monitor:DP-6
-    workspace = 8, monitor:DP-6
-    workspace = 9, monitor:DP-6
-  '';
+        workspace = 1, monitor:DP-6
+        workspace = 2, monitor:DP-8
+        workspace = 3, monitor:DP-8
+        workspace = 4, monitor:DP-8
+        workspace = 5, monitor:DP-8
+        workspace = 6, monitor:DP-6
+        workspace = 7, monitor:DP-6
+        workspace = 8, monitor:DP-6
+        workspace = 9, monitor:DP-6
+      '';
 
   # Home modules to load
   program.hyprlock.defaultMonitor = "DP-8";
