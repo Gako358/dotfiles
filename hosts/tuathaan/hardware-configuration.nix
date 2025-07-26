1fe3b13d5e98db9b4821e1ca7245261df0f150d1
merrinx <gako.footwork856@passinbox.com>
2025-07-26 17:24:59 +0200

    Config powermanagement for laptop

--- hosts/tuathaan/hardware-configuration.nix
+++ hosts/tuathaan/hardware-configuration.nix
@@ -1,4 +1,8 @@
-{ modulesPath, ... }:
+{ modulesPath
+, config
+, lib
+, ...
+}:
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -23,6 +27,34 @@
       "btusb"
     ];
   };
-  # Thunderbolt, a userspace daemon to enable security levels for Thunderbolt 3 on GNU/Linux.
-  services.hardware.bolt.enable = true;
+
+  powerManagement.powertop.enable = true;
+
+  services = {
+    hardware.bolt.enable = true; # Thunderbolt, a userspace daemon to enable security levels for Thunderbolt 3 on GNU/Linux.
+    thermald.enable = true;
+    power-profiles-daemon.enable = lib.mkIf
+      (
+        config.environment.desktop.windowManager == "gnome"
+      )
+      false; # Disable GNOMEs power management
+    system76-scheduler.settings.cfsProfiles.enable = true; # Better scheduling for CPU cycles - thanks System76!!!
+    tlp = {
+      enable = true; # Enable TLP (better than gnomes internal power manager)
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 1;
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+        CPU_HWP_DYN_BOOST_ON_BAT = 1;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 81;
+      };
+    };
+  };
 }
