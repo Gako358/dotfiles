a94739ca25301abbf48737c0200e56183bd9603a
merrinx <gako.footwork856@passinbox.com>
2025-07-26 12:31:18 +0200

    Add thunderbolt service for docking station

--- hosts/tuathaan/hardware-configuration.nix
+++ hosts/tuathaan/hardware-configuration.nix
@@ -1,11 +1,19 @@
-{ modulesPath, ... }: {
+{ modulesPath, ... }:
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
   boot = {
     extraModulePackages = [ ];
     initrd = {
-      availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
+      availableKernelModules = [
+        "xhci_pci"
+        "thunderbolt"
+        "vmd"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+      ];
       kernelModules = [ ];
     };
     kernelModules = [
@@ -15,4 +23,6 @@
       "btusb"
     ];
   };
+  # Thunderbolt, a userspace daemon to enable security levels for Thunderbolt 3 on GNU/Linux.
+  services.hardware.bolt.enable = true;
 }
