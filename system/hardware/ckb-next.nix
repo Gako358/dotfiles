486464f8a105c81994faeaa1152e7166bbb8fe64
merrinx <gako.footwork856@passinbox.com>
2025-09-21 11:32:25 +0200

    Remove dbus menu from ckb

--- system/hardware/ckb-next.nix
+++ system/hardware/ckb-next.nix
@@ -1,8 +1,13 @@
 { config
+, pkgs
 , lib
 , ...
-}: {
+}:
+{
   hardware.ckb-next = lib.mkIf config.environment.gaming.enable {
     enable = true;
+    package = pkgs.ckb-next.overrideAttrs (old: {
+      cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DUSE_DBUS_MENU=0" ];
+    });
   };
 }
