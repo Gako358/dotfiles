92631e6d9d16dafe70e01e2f9f2ea984271095e0
merrinx <gako.footwork856@passinbox.com>
2025-09-26 12:41:58 +0200

    Fix monitor and nvidia license

--- hosts/shitbox/default.nix
+++ hosts/shitbox/default.nix
@@ -8,7 +8,7 @@
   users.users = {
     leif = {
       isNormalUser = true;
-      initialHashedPassword = "$7$CU..../....re.hRJT/dAI5QtAnjQ2or/$nY1JgY1nvyNKZKGsFHnpCJUs9ABZEP5HqbQs04Vqmx4";
+      initialHashedPassword = "$7$CU..../....7emauu/nSIai9Z3k.5nme1$6FDaMoeVeQBls.bZ3FsswOVWoeB.ILPtcIAqZh24f54";
       openssh.authorizedKeys.keys = [
       ];
       extraGroups = [
@@ -23,21 +23,21 @@
   programs.hyprland.settings = lib.mkIf (config.environment.desktop.windowManager == "hyprland") {
     monitor = [
       "eDP-1,1920x1200,2560x1440,1"
-      "DP-8,2560x1440,0x0,1"
-      "DP-6,2560x1440,2560x0,1"
+      "DP-7,2560x1440,0x0,1"
+      "DP-8,2560x1440,2560x0,1"
       ",highrr,auto,1"
     ];
 
     workspace = [
-      "1, monitor:DP-6"
+      "1, monitor:DP-7"
       "2, monitor:DP-8"
       "3, monitor:DP-8"
       "4, monitor:DP-8"
       "5, monitor:DP-8"
-      "6, monitor:DP-6"
-      "7, monitor:DP-6"
-      "8, monitor:DP-6"
-      "9, monitor:DP-6"
+      "6, monitor:DP-7"
+      "7, monitor:DP-8"
+      "8, monitor:DP-8"
+      "9, monitor:DP-7"
     ];
   };
   # Modules loaded
