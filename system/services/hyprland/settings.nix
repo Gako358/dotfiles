ac93d1879906137e8ffbf35c34cbe47a5fa0df64
merrinx <gako.footwork856@passinbox.com>
2025-10-08 20:22:33 +0200

    Fix hyprland animation and blur settings

--- system/services/hyprland/settings.nix
+++ system/services/hyprland/settings.nix
@@ -22,21 +22,58 @@
     general = {
       gaps_in = 7;
       gaps_out = 7;
-      border_size = 1;
+      border_size = 2;
+      "col.active_border" = "rgb(B48EAD) rgb(5E81AC) rgb(719cd6) 45deg";
+      "col.inactive_border" = "rgb(3B4252)";
+      hover_icon_on_border = true;
+      extend_border_grab_area = 15;
       allow_tearing = true;
       resize_on_border = true;
     };
     cursor.inactive_timeout = 5;
     decoration = {
       rounding = 16;
-      blur.enabled = true;
+      blur = {
+        enabled = true;
+        size = 8;
+        passes = 4;
+        new_optimizations = true;
+        ignore_opacity = true;
+        xray = false;
+        contrast = 1.1;
+        brightness = 1.0;
+        noise = 0.02;
+      };
+      active_opacity = 1.0;
+      inactive_opacity = 0.95;
+      fullscreen_opacity = 1.0;
     };
+    layerrule = [
+      "blur, wofi"
+      "ignorealpha 0, wofi"
+      "blur, waybar"
+      "ignorealpha 0, waybar"
+      "blur, notifications"
+      "ignorealpha 0, notifications"
+    ];
     animations.enabled = true;
     animation = [
-      "border, 1, 2, default"
-      "fade, 1, 4, default"
-      "windows, 1, 3, default, popin 80%"
-      "workspaces, 1, 2, default, slide"
+      "windows, 1, 6, wind, slide"
+      "windowsIn, 1, 6, winIn, slide"
+      "windowsOut, 1, 5, winOut, slide"
+      "windowsMove, 1, 5, wind, slide"
+      "border, 1, 10, liner"
+      "borderangle, 1, 60, liner, loop"
+      "fade, 1, 10, default"
+      "workspaces, 1, 6, overshot, slidevert"
+      "specialWorkspace, 1, 6, default, slidevert"
+    ];
+    bezier = [
+      "wind, 0.05, 0.9, 0.1, 1.05"
+      "winIn, 0.1, 1.1, 0.1, 1.1"
+      "winOut, 0.3, -0.3, 0, 1"
+      "liner, 1, 1, 1, 1"
+      "overshot, 0.13, 0.99, 0.29, 1.1"
     ];
     group = {
       groupbar = {
