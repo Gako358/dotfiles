306378b8eef07a608e6961ec5ec7d63776b83082
merrinx <gako.footwork856@passinbox.com>
2025-10-12 12:16:31 +0200

    fix: remove ovmf images from qemu
    All images comes default with qemu, as a package

--- system/programs/qemu.nix
+++ system/programs/qemu.nix
@@ -34,8 +34,9 @@ in
         qemu = {
           package = pkgs.qemu_kvm;
           swtpm.enable = true;
-          ovmf.enable = true;
-          ovmf.packages = [ pkgs.OVMFFull.fd ];
+          # These images comes default with qemu
+          # ovmf.enable = true;
+          # ovmf.packages = [ pkgs.OVMFFull.fd ];
         };
       };
       spiceUSBRedirection.enable = true;
