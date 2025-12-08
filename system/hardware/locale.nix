960f0c06261d3dc678415013efbdecea719b7d5a
merrinx <gako.footwork856@passinbox.com>
2025-12-08 10:22:35 +0100

    feat: Update locale settings

--- system/hardware/locale.nix
+++ system/hardware/locale.nix
@@ -1,8 +1,20 @@
 {
   time.timeZone = "Europe/Oslo";
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-  ];
+  i18n = {
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "nb_NO.UTF-8/UTF-8"
+    ];
+    extraLocaleSettings = {
+      LC_NUMERIC = "nb_NO.UTF-8";
+      LC_TIME = "nb_NO.UTF-8";
+      LC_MONETARY = "nb_NO.UTF-8";
+      LC_ADDRESS = "nb_NO.UTF-8";
+      LC_MEASUREMENT = "nb_NO.UTF-8";
+      LC_PAPER = "nb_NO.UTF-8";
+      LC_TELEPHONE = "nb_NO.UTF-8";
+    };
+  };
 
   # Configure the virtual console keymap from the xserver keyboard settings.
   console = {
