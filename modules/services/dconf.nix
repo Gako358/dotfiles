16ed95c44ecf785e4aab17214da60dde45cae0e8
merrinx <gako.footwork856@passinbox.com>
2025-12-16 17:42:39 +0100

    feat: Removed dash to panel for gnome
    Removed in favour of open bar, vitals and appindicator

--- modules/services/dconf.nix
+++ modules/services/dconf.nix
@@ -92,13 +92,13 @@ in
         "org/gnome/shell" = {
           disable-user-extensions = false;
           enabled-extensions = with pkgs.gnomeExtensions; [
+            appindicator.extensionUuid
             caffeine.extensionUuid
             clipboard-indicator.extensionUuid
-            dash-to-panel.extensionUuid
-            just-perfection.extensionUuid
-            space-bar.extensionUuid
+            open-bar.extensionUuid
             tray-icons-reloaded.extensionUuid
             user-themes.extensionUuid
+            vitals.extensionUuid
           ];
           favorite-apps = [
             "org.gnome.Console.desktop"
@@ -121,64 +121,31 @@ in
           show-notification = false;
           user-enabled = true;
         };
-        "org/gnome/shell/extensions/dash-to-panel" = {
-          window-preview-fixed-x = true;
-          window-preview-fixed-y = true;
-          preview-custom-opacity = 73;
-          window-preview-size = 127;
-          appicon-padding = 5;
-          appicon-margin = 3;
-          show-tooltip = false;
-          show-showdesktop-hover = true;
-          dot-style-unfocused = "DOTS";
-          dot-style-focused = "DOTS";
-          trans-use-custom-opacity = true;
-          trans-panel-opacity = "0.55";
-          tray-size = 15;
-          leftbox-size = 17;
-          panel-element-positions = ''{"AUS-0x0000bec6":[{"element":"showAppsButton","visible":true,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"stackedTL"},{"element":"taskbar","visible":true,"position":"centered"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}'';
-          panel-sizes = "{\"0\":64}";
-          panel-positions = "{\"0\":\"BOTTOM\"}";
-          showdesktop-button-width = "5";
-          show-apps-icon-file = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake-white.svg";
-        };
-        "org/gnome/shell/extensions/just-perfection" = {
-          accessibility-menu = true;
-          app-menu = true;
-          app-menu-icon = true;
-          background-menu = true;
-          clock-menu = false;
-          controls-manager-spacing-size = 22;
-          dash = true;
-          dash-icon-size = 0;
-          double-super-to-appgrid = true;
-          gesture = true;
-          hot-corner = false;
-          notification-banner-position = 2;
-          osd = false;
-          panel = true;
-          panel-arrow = true;
-          panel-corner-size = 1;
-          panel-in-overview = true;
-          panel-notification-icon = true;
-          panel-size = 36;
-          power-icon = true;
-          ripple-box = false;
-          search = false;
-          show-apps-button = true;
-          startup-status = 0;
-          theme = true;
-          window-demands-attention-focus = true;
-          window-picker-icon = false;
-          window-preview-caption = true;
-          window-preview-close-button = true;
-          workspace = true;
-          workspace-background-corner-size = 15;
-          workspace-popup = false;
-          workspaces-in-app-grid = true;
+        "org/gnome/shell/extensions/openbar" = {
+          autotheme-dark = "Dark";
+          autotheme-light = "Dark";
+          bartype = "Islands";
+          dashdock-style = "Bar";
+          autotheme-refresh = true;
+          trigger-autotheme = true;
+          margin = 1.0;
+          height = 35.0;
+          bradius = 5.0;
+          dbradius = 5.0;
+          isalpha = 0.71999999999999997;
         };
         "org/gnome/shell/extensions/user-theme" = {
           name = "palenight";
+        };
+        "org/gnome/shell/extensions/vitals" = {
+          hot-sensors = [
+            "_processor_usage_"
+            "_memory_usage_"
+          ];
+          position-in-panel = 0;
+          use-higher-precision = false;
+          alphabetize = true;
+          hide-zeros = false;
         };
         "org/gnome/shell/keybindings" = {
           switch-to-application-1 = [ ];
@@ -211,14 +178,14 @@ in
         );
 
         packages = with pkgs; [
+          gnomeExtensions.appindicator
           gnomeExtensions.caffeine
           gnomeExtensions.clipboard-indicator
-          gnomeExtensions.dash-to-panel
-          gnomeExtensions.just-perfection
-          gnomeExtensions.space-bar
+          gnomeExtensions.open-bar
           gnomeExtensions.sound-output-device-chooser
           gnomeExtensions.tray-icons-reloaded
           gnomeExtensions.user-themes
+          gnomeExtensions.vitals
           palenight-theme
         ];
       };
