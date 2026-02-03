be3db81010f285b71de885b386c99f6fb3872021
merrinx <gako.footwork856@passinbox.com>
2026-02-03 12:24:35 +0100

    fix: Locale settings set to UTF-8

--- system/hardware/locale.nix
+++ system/hardware/locale.nix
@@ -1,11 +1,14 @@
 {
   time.timeZone = "Europe/Oslo";
   i18n = {
+    defaultLocale = "en_US.UTF-8";
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
       "nb_NO.UTF-8/UTF-8"
     ];
     extraLocaleSettings = {
+      LC_ALL = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
       LC_NUMERIC = "nb_NO.UTF-8";
       LC_TIME = "nb_NO.UTF-8";
       LC_MONETARY = "nb_NO.UTF-8";
