42bd301d9eb1e1959a1cb495b713e0deecf19a32
merrinx <gako.footwork856@passinbox.com>
2026-03-03 15:23:30 +0100

    fix: Update monitor settings for gnome

--- modules/profiles/tanchico/default.nix
+++ modules/profiles/tanchico/default.nix
@@ -53,6 +53,50 @@ in
             </monitor>
           </logicalmonitor>
         </configuration>
+        <configuration>
+          <layoutmode>physical</layoutmode>
+          <logicalmonitor>
+            <x>0</x>
+            <y>277</y>
+            <scale>1</scale>
+            <transform>
+              <rotation>left</rotation>
+              <flipped>no</flipped>
+            </transform>
+            <monitor>
+              <monitorspec>
+                <connector>HDMI-1</connector>
+                <vendor>AUS</vendor>
+                <product>ASUS VG249</product>
+                <serial>0x00034efa</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.000</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+          <logicalmonitor>
+            <x>1080</x>
+            <y>0</y>
+            <scale>1</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>DP-2</connector>
+                <vendor>AUS</vendor>
+                <product>ASUS VP28U</product>
+                <serial>0x0000bec6</serial>
+              </monitorspec>
+              <mode>
+                <width>3840</width>
+                <height>2160</height>
+                <rate>59.997</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
       </monitors>
     '';
   };
