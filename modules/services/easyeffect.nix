0a31f916aa989fb918e46c5530d1c053ee320382
merrinx <gako.footwork856@passinbox.com>
2026-03-28 21:24:30 +0100

    fix(easyeffect): Change vad-thres for noise cancellation to 91

--- modules/services/easyeffect.nix
+++ modules/services/easyeffect.nix
@@ -2,29 +2,31 @@
 , lib
 , ...
 }:
+let
+  inherit (osConfig.environment) desktop;
+in
 {
-  services.easyeffects = lib.mkIf osConfig.environment.desktop.enable {
+  services.easyeffects = lib.mkIf desktop.enable {
     enable = true;
     preset = "noise-cancellation";
   };
 
-  xdg.configFile."easyeffects/input/noise-cancellation.json" =
-    lib.mkIf osConfig.environment.desktop.enable
-      {
-        text = builtins.toJSON {
-          input = {
-            blocklist = [ ];
-            plugins_order = [ "rnnoise#0" ];
-            "rnnoise#0" = {
-              bypass = false;
-              enable-vad = true;
-              input-gain = 0.0;
-              model-name = "";
-              output-gain = 0.0;
-              release = 20.0;
-              vad-thres = 50.0;
-            };
-          };
+  xdg.configFile."easyeffects/input/noise-cancellation.json" = lib.mkIf desktop.enable {
+    text = builtins.toJSON {
+      input = {
+        blocklist = [ ];
+        plugins_order = [ "rnnoise#0" ];
+        "rnnoise#0" = {
+          bypass = false;
+          enable-vad = true;
+          input-gain = 0.0;
+          model-name = "";
+          output-gain = 0.0;
+          release = 20.0;
+          vad-thres = 91.0;
+          wet = 0.0;
         };
       };
+    };
+  };
 }
