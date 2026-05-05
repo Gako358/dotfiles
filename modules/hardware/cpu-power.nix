8ea4fd54f905821bed9fb347a91aca5e4aa15491
merrinx <gako.footwork856@passinbox.com>
2026-05-05 20:52:04 +0200

    feat: Add cpu-power rules for desktop amd

---
+++ modules/hardware/cpu-power.nix
@@ -0,0 +1,26 @@
+_: {
+  flake.nixosModules.hardware-cpu-power =
+    {
+      config,
+      lib,
+      ...
+    }:
+    let
+      isAmd = lib.elem "kvm-amd" config.boot.kernelModules;
+    in
+    {
+      config = lib.mkIf config.environment.desktop.enable {
+        boot.kernelParams = lib.mkIf isAmd [ "amd_pstate=guided" ];
+
+        powerManagement = {
+          cpuFreqGovernor = lib.mkDefault "powersave";
+          powertop.enable = true;
+        };
+
+        systemd.tmpfiles.rules = [
+          "w /sys/devices/system/cpu/cpufreq/boost - - - - 0"
+          "w /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference - - - - power"
+        ];
+      };
+    };
+}
