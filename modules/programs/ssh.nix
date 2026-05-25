e45ccb92d074d2862efce5a07ba629ba26634ab3
merrinx <gako.footwork856@passinbox.com>
2026-05-25 13:09:23 +0200

    refactor(ssh): migrate matchBlocks to settings for SSH config options

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -4,20 +4,20 @@ _: {
       enable = true;
 
       enableDefaultConfig = false;
-      matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "yes";
-        compression = true;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-      };
+      settings = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "yes";
+          compression = true;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
 
-      matchBlocks = {
         "github.com" = {
           # "Using SSH over the HTTPS port for GitHub"
           # "(port 22 is banned by some proxies / firewalls)"
