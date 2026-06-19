bc4bd078bc1b91c7d6520d85ed0cedd67b293336
merrinx <gako.footwork856@passinbox.com>
2026-06-19 20:04:47 +0200

    build(hardware/ckb-next): update ckb-next to latest unstable git version
    - Bump ckb-next to 0.6.2-unstable-2026-06-02 with new git revision
    - Update source hash and overrideAttrs for custom build
    - Refresh flake.lock dependencies for latest upstream changes

--- modules/hardware/ckb-next.nix
+++ modules/hardware/ckb-next.nix
@@ -11,7 +11,14 @@ _: {
       config = lib.mkIf config.environment.gaming.enable {
         hardware.ckb-next = {
           enable = true;
-          package = pkgs.ckb-next.overrideAttrs (old: {
+          package = pkgs.ckb-next.overrideAttrs (old: rec {
+            version = "0.6.2-unstable-2026-06-02";
+            src = pkgs.fetchFromGitHub {
+              owner = "ckb-next";
+              repo = "ckb-next";
+              rev = "5edd3c14810c1aa93be5adde471071b97ef108b4";
+              hash = "sha256-h5jZqiK4hYqlLFPY9jmJEniarSHE4ZcoIX4Qv3QdELc=";
+            };
             cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DUSE_DBUS_MENU=0" ];
           });
         };
