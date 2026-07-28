d9a75e2466d71c2b7b5dbf890f724d43a140f105
merrinx <gako.footwork856@passinbox.com>
2026-07-28 10:14:35 +0200

    refactor(scramgit): migrate to home module and improve repo handling
    - Move scramgit module from nixosModules to homeModules
    - Refactor repo list to use a map for easier management
    - Add dynamic flag generation for scramgit command
    - Provide scramgit as a shell script in home.packages
    - Update flake.lock for new dependencies and scramgit input

--- modules/services/scramgit.nix
+++ modules/services/scramgit.nix
@@ -1,20 +1,77 @@
 _: {
-  flake.nixosModules.services-scramgit =
-    { pkgs, inputs, ... }:
+  flake.homeModules.services-scramgit =
     {
-      environment.systemPackages = [
-        inputs.scramgit.defaultPackage.${pkgs.stdenv.hostPlatform.system}
-      ];
+      config,
+      lib,
+      pkgs,
+      inputs,
+      ...
+    }:
+    let
+      owner = "Gako358";
 
-      # TODO: Need to fix env for python, and shell to run scram
-      # systemd.user.services.scramgit = {
-      #   description = "Run scramgit to update repositories";
-      #   wantedBy = [ "default.target" ];
-      #   after = [ "graphical-session.target" ];
-      #   serviceConfig = {
-      #     Type = "oneshot";
-      #     ExecStart = "${inputs.scramgit.defaultPackage.${pkgs.stdenv.hostPlatform.system}}/bin/scramgit";
-      #   };
-      # };
+      repos = {
+        "101-rs" = "Projects/101-rs";
+        ".password-store" = ".password-store";
+        agentx = "Sources/agentx";
+        archive = "Sources/archive";
+        bivrost = "Projects/bivrost";
+        books = "Documents/books";
+        "bootstrap.nix" = "Projects/bootstrap.nix";
+        "borealis.nvim" = "Projects/plugins/borealis.nvim";
+        classified = "Documents/classified";
+        discologs = "Projects/discologs";
+        dotfiles = "Sources/dotfiles";
+        dreamer = "Projects/plugins/dreamer";
+        dwm = "Projects/suckless/dwm";
+        elmugge = "Projects/elmugge";
+        emacs-flake = "Projects/emacs-flake";
+        flakeUI = "Projects/flakeUI";
+        gako358 = "Projects/gako358";
+        interviews = "Projects/interviews";
+        jamocha = "Projects/wwwsite/jamocha";
+        jrt = "Projects/jrt";
+        leetcode = "Projects/leetcode";
+        Masterplan = "Projects/Masterplan";
+        mXoverlay = "Projects/mXoverlay";
+        mxgitbucket = "Projects/mxgitbucket";
+        neovim = "Projects/neovim";
+        notes = "Documents/notes";
+        "nvim-chatGPT" = "Projects/plugins/nvim-chatGPT";
+        ProjectOS = "Projects/ProjectOS";
+        qregAPI = "Projects/wwwsite/openQregX/qregAPI";
+        qregData = "Projects/wwwsite/openQregX/qregData";
+        qregUI = "Projects/wwwsite/openQregX/qregUI";
+        Reports = "Documents/reports";
+        research = "Projects/research";
+        rockjvm = "Projects/rockjvm";
+        rustacean = "Projects/rustacean";
+        ruststrom = "Projects/ruststrom";
+        schemeQT = "Projects/schemeqt";
+        Scram = "Projects/scram";
+        "sessions.nvim" = "Projects/plugins/sessions.nvim";
+        slock = "Projects/suckless/slock";
+        snake = "Projects/snake";
+        socrates = "Projects/socrates";
+        st = "Projects/suckless/st";
+        standup = "Projects/standup";
+        vsmugge = "Projects/vsmugge";
+      };
+
+      flags = lib.concatLists (
+        lib.mapAttrsToList (repo: path: [
+          "--repo"
+          "git@github.com:${owner}/${repo}.git=${config.home.homeDirectory}/${path}"
+        ]) repos
+      );
+
+      scramgit = inputs.scramgit.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    in
+    {
+      home.packages = [
+        (pkgs.writeShellScriptBin "scramgit" ''
+          exec ${lib.getExe scramgit} ${lib.escapeShellArgs flags} "$@"
+        '')
+      ];
     };
 }
